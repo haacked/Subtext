0b69da7e9a6f641f2a6686d81e7270a0cf4ba4ad
haacked <haacked@483c6c4e-18e1-11de-b208-e5a9466b8568>
2005-05-23 03:56:07 +0000

    Implemented multiple blogs. Work in progress.

---
+++ SubtextSystem/SubtextSolution/UnitTests.Subtext/Framework/Configuration/SingleBlogConfigTests.cs
@@ -0,0 +1,14 @@
+using System;
+using NUnit.Framework;
+
+namespace UnitTests.Subtext.Framework.Configuration
+{
+	/// <summary>
+	/// Tests of the SingleBLogConfig helper.
+	/// </summary>
+	[TestFixture]
+	public class SingleBlogConfigTests
+	{
+		
+	}
+}
---
+++ SubtextSystem/SubtextSolution/UnitTests.Subtext/Framework/Format/UrlFormatsTests.cs
@@ -0,0 +1,55 @@
+using System;
+using NUnit.Framework;
+using Subtext.Framework.Format;
+
+namespace UnitTests.Subtext.Framework.Format
+{
+	/// <summary>
+	/// Tests of the <see cref="UrlFormats"/> class.
+	/// </summary>
+	[TestFixture]
+	public class UrlFormatsTests
+	{
+		/// <summary>
+		/// Makes sure that UrlFormats.GetBlogAppFromRequest does the right thing.
+		/// </summary>
+		[Test]
+		public void GetBlogAppFromRequestDoesTheRightThing()
+		{
+			string rawUrl = "/Subtext.Web/MyBlog/default.aspx";
+			string app = "/Subtext.Web";
+
+			Assert.AreEqual("MyBlog", UrlFormats.GetBlogAppFromRequest(rawUrl, app));
+		
+			rawUrl = "/subtext.web/MyBlog/default.aspx";
+			app = "/Subtext.Web";
+
+			Assert.AreEqual("MyBlog", UrlFormats.GetBlogAppFromRequest(rawUrl, app));
+
+			rawUrl = "/subtext.web/MyBlog/default.aspx";
+			app = "Subtext.Web";
+
+			Assert.AreEqual("MyBlog", UrlFormats.GetBlogAppFromRequest(rawUrl, app));
+
+			rawUrl = "/subtext.web/default.aspx";
+			app = "/Subtext.Web";
+
+			Assert.AreEqual(string.Empty, UrlFormats.GetBlogAppFromRequest(rawUrl, app));
+
+			rawUrl = "/subtext.web";
+			app = "/Subtext.Web";
+
+			Assert.AreEqual(string.Empty, UrlFormats.GetBlogAppFromRequest(rawUrl, app));
+
+			rawUrl = "/subtext.web/myBLOG";
+			app = "/Subtext.Web";
+
+			Assert.AreEqual("myBLOG", UrlFormats.GetBlogAppFromRequest(rawUrl, app));
+
+			rawUrl = "/subtext.web/myBLOG";
+			app = "/Subtext.Web/";
+
+			Assert.AreEqual("myBLOG", UrlFormats.GetBlogAppFromRequest(rawUrl, app));
+		}
+	}
+}
