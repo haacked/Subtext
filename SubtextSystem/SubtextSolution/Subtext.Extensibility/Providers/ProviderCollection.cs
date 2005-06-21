2ce389dafb96109a8ddd035514d1a297a6963db7
haacked <haacked@483c6c4e-18e1-11de-b208-e5a9466b8568>
2005-06-21 23:31:46 +0000

    *** empty log message ***

---
+++ SubtextSystem/SubtextSolution/Subtext.Extensibility/Providers/ProviderCollection.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections;
+
+namespace Subtext.Extensibility.Providers
+{
+	/// <summary>
+	/// Summary description for ProviderCollection.
+	/// </summary>
+	public class ProviderCollection : CollectionBase
+	{
+		
+	}
+}
