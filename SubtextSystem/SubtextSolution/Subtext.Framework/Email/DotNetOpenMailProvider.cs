f64815e2eab97a40a682410462c6718ab33c6d22
haacked <haacked@483c6c4e-18e1-11de-b208-e5a9466b8568>
2005-08-06 19:00:57 +0000

    Implemented a new Email Provider, the DotNetOpenEmail (http://dotnetopenmail.sourceforge.net/).

---
+++ SubtextSystem/SubtextSolution/Subtext.Framework/Email/DotNetOpenMailProvider.cs
@@ -0,0 +1,41 @@
+using System;
+using DotNetOpenMail;
+using DotNetOpenMail.SmtpAuth;
+using Subtext.Extensibility.Providers;
+
+namespace Subtext.Framework.Email
+{
+	/// <summary>
+	/// Implements the <see cref="EmailProvider"/> using an open source library, 
+	/// <see href="http://dotnetopenmail.sourceforge.net/">DotNetOpenMail</see>. 
+	/// </summary>
+	public class DotNetOpenMailProvider : EmailProviderBase
+	{
+		/// <summary>
+		/// Sends an email with the specified parameters.
+		/// </summary>
+		/// <param name="to">Email address of the recipient.</param>
+		/// <param name="from">Email address of the sender.</param>
+		/// <param name="subject">Subject of the email.</param>
+		/// <param name="message">The body of the email Message.</param>
+		/// <returns></returns>
+		public override bool Send(string to, string from, string subject, string message)
+		{
+			EmailMessage email = new EmailMessage();
+			email.FromAddress = new EmailAddress(from);
+			email.AddToAddress(new EmailAddress(to));
+			email.Subject = subject;
+			email.TextPart = new TextAttachment(message);
+
+			SmtpServer smtpServer = new SmtpServer(SmtpServer);
+			
+			//Authentication.
+			if(this.UserName != null && this.Password != null)
+			{
+				smtpServer.SmtpAuthToken = new SmtpAuthToken(UserName, Password);
+			}
+
+			return email.Send(smtpServer);
+		}
+	}
+}
--- SubtextSystem/SubtextSolution/Subtext.Framework/Email/SystemMailProvider.cs
+++ SubtextSystem/SubtextSolution/Subtext.Framework/Email/SystemMailProvider.cs
@@ -22,47 +22,18 @@
 #endregion
 
 using System;
-using System.Collections.Specialized;
 using System.Web.Mail;
 using Subtext.Extensibility.Providers;
 
 namespace Subtext.Framework.Email
 {
 	/// <summary>
-	/// Default implementation of the <see cref="EmailProvider"/>
+	/// Default implementation of the <see cref="EmailProvider"/>.  This uses 
+	/// classes in the System.Web.Mail namespace which have dependencies on 
+	/// CDONTS.
 	/// </summary>
-	public class SystemMailProvider : EmailProvider
+	public class SystemMailProvider : EmailProviderBase
 	{
-		string _name;
-
-		private string _adminEmail;
-		public override string AdminEmail
-		{
-			get{return _adminEmail;}
-			set{_adminEmail = value;}
-		}
-
-		private string _smtpServer = "localhost";
-		public override string SmtpServer
-		{
-			get{return _smtpServer;}
-			set{_smtpServer = value;}
-		}
-
-		private string _password;
-		public override string Password
-		{
-			get{return _password;}
-			set{_password = value;}
-		}
-
-		private string _userName;
-		public override string UserName
-		{
-			get{return _userName;}
-			set{_userName = value;}
-		}
-
 		public override bool Send(string to, string from, string subject, string message)
 		{
 			try
@@ -73,7 +44,8 @@ namespace Subtext.Framework.Email
 				em.Subject = subject;
 				em.Body = message;
 
-				//Found out how to send authenticated email via System.Web.Mail at http://SystemWebMail.com (fact 3.8)
+				// Found out how to send authenticated email via System.Web.Mail 
+				// at http://SystemWebMail.com (fact 3.8)
 				if(this.UserName != null && this.Password != null)
 				{
 					em.Fields.Add("http://schemas.microsoft.com/cdo/configuration/smtpauthenticate", "1");	//basic authentication
@@ -89,27 +61,6 @@ namespace Subtext.Framework.Email
 			{
 				return false;
 			}
-		}
-
-		/// <summary>
-		/// Initializes the specified provider.
-		/// </summary>
-		/// <param name="name">Friendly Name of the provider.</param>
-		/// <param name="configValue">Config value.</param>
-		public override void Initialize(string name, NameValueCollection configValue)
-		{
-			_name = name;
-			_adminEmail = configValue["adminEmail"];
-			_smtpServer = configValue["smtpServer"];
-		}
-
-		/// <summary>
-		/// Returns the friendly name of the provider when the provider is initialized.
-		/// </summary>
-		/// <value></value>
-		public override string Name
-		{
-			get { return _name;}
 		}
 	}
 }
