55ad46e25032dc3e0b0ff4abce51cfba8ea09216
haacked <haacked@483c6c4e-18e1-11de-b208-e5a9466b8568>
2005-10-21 18:34:39 +0000

    Implemented a simple content based spam filter. Very simple. Just a stop gap.

--- SubtextSystem/SubtextSolution/Subtext.Framework/Exceptions/BaseCommentException.cs
+++ SubtextSystem/SubtextSolution/Subtext.Framework/Exceptions/BaseCommentException.cs
@@ -5,7 +5,38 @@ namespace Subtext.Framework.Exceptions
 	/// <summary>
 	/// Base exception class for comment errors.
 	/// </summary>
+	[Serializable]
 	public abstract class BaseCommentException : BaseSubtextException
 	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="CommentSpamException"/> class.
+		/// </summary>
+		public BaseCommentException() : base()
+		{
+		}
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="CommentSpamException"/> class.
+		/// </summary>
+		/// <param name="message">The message.</param>
+		public BaseCommentException(string message) : base(message)
+		{
+		}
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="BaseCommentException"/> class.
+		/// </summary>
+		/// <param name="innerException">The inner exception.</param>
+		public BaseCommentException(Exception innerException) : base(innerException)
+		{}
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="CommentSpamException"/> class.
+		/// </summary>
+		/// <param name="message">The message.</param>
+		/// <param name="innerException">The inner exception.</param>
+		public BaseCommentException(string message, Exception innerException) : base(message, innerException)
+		{
+		}
 	}
 }
