440241a500ded13e2c1e90ac6662bbdb9c206448
haacked <haacked@483c6c4e-18e1-11de-b208-e5a9466b8568>
2006-01-13 03:55:05 +0000

    Password change page for the HostAdmin.

---
+++ SubtextSystem/SubtextSolution/Subtext.Web/HostAdmin/ChangePassword.aspx.cs
@@ -0,0 +1,42 @@
+using System;
+
+namespace Subtext.Web.HostAdmin
+{
+	/// <summary>
+	/// Allows the user to change the host admin password.
+	/// </summary>
+	public class ChangePassword : System.Web.UI.Page
+	{
+		protected Subtext.Web.Controls.ContentRegion MPTitle;
+		protected Subtext.Web.Controls.ContentRegion MPSubTitle;
+		protected Subtext.Web.Controls.MasterPage MPContainer;
+		protected Subtext.Web.Controls.ContentRegion MPSectionTitle;
+		protected System.Web.UI.HtmlControls.HtmlAnchor lnkHostAdmin;
+	
+		private void Page_Load(object sender, System.EventArgs e)
+		{
+			// Put user code to initialize the page here
+		}
+
+		#region Web Form Designer generated code
+		override protected void OnInit(EventArgs e)
+		{
+			//
+			// CODEGEN: This call is required by the ASP.NET Web Form Designer.
+			//
+			InitializeComponent();
+			base.OnInit(e);
+		}
+		
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{    
+			this.Load += new System.EventHandler(this.Page_Load);
+
+		}
+		#endregion
+	}
+}
