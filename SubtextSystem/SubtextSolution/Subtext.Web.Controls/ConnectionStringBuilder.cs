aad433123eccfb40ef94fea268eb5c01e4ac405e
simo <simo@483c6c4e-18e1-11de-b208-e5a9466b8568>
2006-01-19 21:09:47 +0000

    Advanced connection string builder.... compiles, but doesn't work

--- SubtextSystem/SubtextSolution/Subtext.Web.Controls/ConnectionStringBuilder.cs
+++ SubtextSystem/SubtextSolution/Subtext.Web.Controls/ConnectionStringBuilder.cs
@@ -3,6 +3,11 @@ using System.ComponentModel;
 using System.Web.UI;
 using System.Web.UI.HtmlControls;
 using System.Web.UI.WebControls;
+using Subtext.Scripting;
+using System.Collections;
+using SQLDMO;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace Subtext.Web.Controls
 {
@@ -10,12 +15,25 @@ namespace Subtext.Web.Controls
 	/// Control used to specify a connection string.
 	/// </summary>
 	[ValidationProperty("ConnectionString")]
-	public class ConnectionStringBuilder : System.Web.UI.WebControls.WebControl, INamingContainer
+	public class ConnectionStringBuilder : Control, INamingContainer
 	{
 		const string ConnectionStringControlId = "txtConnectionStringBuilderConnectionString";
 		const string DescriptionControlId = "ltlConnectionStringBuilderDescriptionText";
 		const string TitleControlId = "ltlConnectionStringBuilderTitleText";
 		const string CheckboxControlId = "chkUseConnectionStringInWebConfig";
+
+		protected System.Web.UI.WebControls.DropDownList machineName;
+		protected System.Web.UI.WebControls.TextBox otherMachineName;
+		protected System.Web.UI.WebControls.RadioButtonList authMode;
+		protected System.Web.UI.WebControls.TextBox username;
+		protected System.Web.UI.WebControls.DropDownList databaseName;
+		protected System.Web.UI.WebControls.Button testConnection;
+		protected System.Web.UI.WebControls.TextBox password;
+		protected System.Web.UI.WebControls.Label connResult;
+		protected System.Web.UI.WebControls.LinkButton refreshDatabase;
+
+
+		private ConnectionString _connStr=Subtext.Scripting.ConnectionString.Empty;
 
 		/// <summary>
 		/// Called during the page init event.
@@ -55,9 +73,20 @@ namespace Subtext.Web.Controls
 			HtmlTableRow row = new HtmlTableRow();
 			row.VAlign = "top";
 			HtmlTableCell questionCell = new HtmlTableCell();
-			TextBox textbox = new TextBox();
-			textbox.ID = ConnectionStringControlId;
-			questionCell.Controls.Add(textbox);
+
+			// Verify is SQL-DMO is installed
+			if(CheckSQLDMO()) 
+			{
+				//Build advanced control
+				BuildAdvancedBuilder();
+			}
+			else 
+			{
+				//Build low-level Textbox
+				TextBox textbox = new TextBox();
+				textbox.ID = ConnectionStringControlId;
+				questionCell.Controls.Add(textbox);
+			}
 			row.Cells.Add(questionCell);
 
 			//Checkbox to use connection string in web.config
@@ -67,7 +96,7 @@ namespace Subtext.Web.Controls
 				checkbox.ID = CheckboxControlId;
 				checkbox.Text = "Use Connection String In Web.config";
 				//This next line is a hack since we don't yet know the client id yet.
-				checkbox.Attributes["onclick"] = "if(this.checked) {" + this.ID + "_" + textbox.ClientID + ".disabled = true;} else {" + this.ID + "_" + textbox.ClientID + ".disabled = false;} ;";
+				//checkbox.Attributes["onclick"] = "if(this.checked) {" + this.ID + "_" + textbox.ClientID + ".disabled = true;} else {" + this.ID + "_" + textbox.ClientID + ".disabled = false;} ;";
 				questionCell.Controls.Add(new LiteralControl("<br />"));
 				questionCell.Controls.Add(checkbox);
 			}
@@ -81,6 +110,339 @@ namespace Subtext.Web.Controls
 			table.Rows.Add(row);
 			this.Controls.Add(table);
 			base.CreateChildControls();
+		}
+
+		private bool CheckSQLDMO() 
+		{
+			return true;
+		}
+
+		/// <summary>
+		/// Build the advanced Connection String Builder
+		/// Build the UI, attach events, and populate the fields
+		/// </summary>
+		private void BuildAdvancedBuilder() 
+		{
+			BuildMainTable();
+			AttachEvents();
+			LoadData();
+		}
+
+
+		#region UI Builder
+		/// <summary>
+		/// Build the main UI Table
+		/// </summary>
+		private void BuildMainTable() 
+		{
+			HtmlTable mainTable = new HtmlTable();
+			mainTable.Rows.Add(BuildServerNameRow());
+			mainTable.Rows.Add(BuildAuthenticationRow());
+			mainTable.Rows.Add(BuildUsernameRow());
+			mainTable.Rows.Add(BuildPasswordRow());
+			mainTable.Rows.Add(BuildDatabaseRow());
+			mainTable.Rows.Add(BuildTestConnRow());
+			Controls.Add(mainTable);
+			
+		}
+
+		private HtmlTableRow BuildServerNameRow()
+		{
+			HtmlTableRow row = new HtmlTableRow();
+			HtmlTableCell cell;
+
+			cell=new HtmlTableCell();
+			cell.InnerHtml="Server Name";
+			row.Cells.Add(cell);
+
+			cell=new HtmlTableCell();
+			machineName = new DropDownList();
+			otherMachineName= new TextBox();
+			otherMachineName.TextMode=TextBoxMode.SingleLine;
+			cell.Controls.Add(machineName);
+			cell.Controls.Add(new HtmlGenericControl("br"));
+			cell.Controls.Add(otherMachineName);
+			row.Cells.Add(cell);
+
+			return row;
+		}
+
+		private HtmlTableRow BuildAuthenticationRow()
+		{
+			HtmlTableRow row = new HtmlTableRow();
+			HtmlTableCell cell;
+
+			cell=new HtmlTableCell();
+			cell.InnerHtml="Authentication";
+			row.Cells.Add(cell);
+
+			cell=new HtmlTableCell();
+			authMode= new RadioButtonList();
+			authMode.AutoPostBack=true;
+			authMode.Items.Add(new ListItem("SQL","sql"));
+			authMode.Items.Add(new ListItem("Trusted","win"));
+			cell.Controls.Add(authMode);
+			row.Cells.Add(cell);
+
+			return row;
+		}
+
+		private HtmlTableRow BuildUsernameRow()
+		{
+			HtmlTableRow row = new HtmlTableRow();
+			HtmlTableCell cell;
+
+			cell=new HtmlTableCell();
+			cell.InnerHtml="Username";
+			row.Cells.Add(cell);
+
+			cell=new HtmlTableCell();
+			username= new TextBox();
+			username.TextMode=TextBoxMode.SingleLine;
+			cell.Controls.Add(username);
+			row.Cells.Add(cell);
+
+			return row;
+		}
+
+		private HtmlTableRow BuildPasswordRow()
+		{
+			HtmlTableRow row = new HtmlTableRow();
+			HtmlTableCell cell;
+
+			cell=new HtmlTableCell();
+			cell.InnerHtml="Password";
+			row.Cells.Add(cell);
+
+			cell=new HtmlTableCell();
+			password= new TextBox();
+			password.TextMode=TextBoxMode.SingleLine;
+			cell.Controls.Add(password);
+			row.Cells.Add(cell);
+
+			return row;
+		}
+
+		private HtmlTableRow BuildDatabaseRow()
+		{
+			HtmlTableRow row = new HtmlTableRow();
+			HtmlTableCell cell;
+
+			cell=new HtmlTableCell();
+			cell.InnerHtml="Database";
+			row.Cells.Add(cell);
+
+			cell=new HtmlTableCell();
+			databaseName= new DropDownList();
+			refreshDatabase=new LinkButton();
+			refreshDatabase.Text="Refresh Databases";
+			cell.Controls.Add(databaseName);
+			cell.Controls.Add(refreshDatabase);
+			row.Cells.Add(cell);
+
+			return row;
+		}
+
+		private HtmlTableRow BuildTestConnRow()
+		{
+			HtmlTableRow row = new HtmlTableRow();
+			HtmlTableCell cell;
+
+			cell=new HtmlTableCell();
+			cell.ColSpan=2;
+			testConnection= new Button();
+			testConnection.Text="Test Connection";
+			connResult= new Label();
+			connResult.Text="";
+			cell.Controls.Add(testConnection);
+			cell.Controls.Add(new HtmlGenericControl("br"));
+			cell.Controls.Add(connResult);
+			row.Cells.Add(cell);
+
+			return row;
+		}
+
+		#endregion // UI Builder
+
+		/// <summary>
+		/// Attach Events to the WebControls
+		/// </summary>
+		private void AttachEvents() 
+		{
+			authMode.SelectedIndexChanged += new System.EventHandler(this.authMode_SelectedIndexChanged);
+			testConnection.Click += new System.EventHandler(this.testConnection_Click);
+			refreshDatabase.Click += new System.EventHandler(this.refreshDatabase_Click);
+		}
+
+		/// <summary>
+		/// Populate web controls with inital values
+		/// If no connection string provided it just populate a combobox with
+		/// all discovered SQL Servers
+		/// </summary>
+		private void LoadData()
+		{
+			connResult.Text="";
+			if(!Page.IsPostBack) 
+			{
+				PopulateServerNameCmb();
+				if(_connStr!=null) 
+				{
+					if(machineName.Items.FindByValue(_connStr.Server)!=null)
+						machineName.SelectedValue=_connStr.Server;
+					else
+						otherMachineName.Text=_connStr.Server;
+
+					if(_connStr.TrustedConnection)
+					{
+						authMode.SelectedValue="win";
+						username.Enabled=false;
+						password.Enabled=false;
+					}
+					else 
+					{
+						authMode.SelectedValue="sql";
+						username.Enabled=true;
+						password.Enabled=true;
+						username.Text=_connStr.UserId;
+						password.Text=_connStr.Password;
+					}
+
+					PopulateDatabaseNamesCmb(_connStr);
+				}
+			}
+		}
+
+
+
+		/// <summary>
+		/// Uses SQL-DMO to retrieve all servers available on the network
+		/// </summary>
+		private void PopulateServerNameCmb() 
+		{
+			ArrayList serverNames = new ArrayList();
+			SQLDMO.Application dmo = new SQLDMO.ApplicationClass();
+			SQLDMO.NameList instances = dmo.ListAvailableSQLServers();
+			foreach(string instance in instances) 
+			{
+				serverNames.Add(instance);
+			}
+			machineName.DataSource=serverNames;
+			machineName.DataBind();
+		}
+
+
+
+		/// <summary>
+		/// Populate the databases dropdown list with the DBs found on the specified server
+		/// </summary>
+		/// <param name="connStr">ConnectionString to use for listing DBs</param>
+		private void PopulateDatabaseNamesCmb(ConnectionString connStr)
+		{
+			ArrayList dbNames = new ArrayList();
+			SQLDMO.SQLServer sqlInstance = new SQLDMO.SQLServerClass();
+			try
+			{
+				if(_connStr.TrustedConnection) 
+				{
+					sqlInstance.LoginSecure=true;
+					sqlInstance.Connect(_connStr.Server,null,null);
+				}
+				else
+					sqlInstance.Connect(_connStr.Server,_connStr.UserId,_connStr.Password);
+				foreach(SQLDMO.Database db in sqlInstance.Databases) 
+				{
+					dbNames.Add(db.Name);
+				}
+				databaseName.DataSource=dbNames;
+				databaseName.DataBind();
+
+				if(databaseName.Items.FindByValue(_connStr.Database)!=null) 
+					databaseName.SelectedValue=_connStr.Database;
+			}
+			catch(Exception) 
+			{
+				connResult.Text="Error retrieving database list";
+				databaseName.Items.Clear();
+				databaseName.Items.Add(new ListItem("-- Error retrieving database list --","0"));
+			}
+			finally
+			{
+				sqlInstance.DisConnect();
+			}
+		}
+
+		
+		private void SetConnectionString()
+		{
+			_connStr.TrustedConnection = authMode.SelectedValue.Equals("win");
+			if(machineName.SelectedIndex!=-1)
+				_connStr.Server = machineName.SelectedValue;
+			else
+				_connStr.Server = otherMachineName.Text;
+			_connStr.Database = databaseName.SelectedValue;
+			
+			if(!_connStr.TrustedConnection)
+			{
+				_connStr.UserId = username.Text;
+				_connStr.Password = password.Text;
+			}
+			else
+			{
+				_connStr.UserId = string.Empty;
+				_connStr.Password = string.Empty;
+			}			
+		}
+
+		private void authMode_SelectedIndexChanged(object sender, System.EventArgs e)
+		{
+			if(authMode.SelectedValue.Equals("win")) 
+			{
+				username.Enabled=false;
+				password.Enabled=false;
+				SetConnectionString();
+				PopulateDatabaseNamesCmb(_connStr);
+			}
+			else if(authMode.SelectedValue.Equals("sql")) 
+			{
+				username.Enabled=true;
+				password.Enabled=true;
+				username.Text=_connStr.UserId;
+				password.Text=_connStr.Password;
+				if(!username.Text.Trim().Equals(String.Empty))
+				{
+					PopulateDatabaseNamesCmb(_connStr);
+				}
+			}
+		}
+
+		private void refreshDatabase_Click(object sender, System.EventArgs e)
+		{
+			SetConnectionString();
+			connResult.Text="";
+			PopulateDatabaseNamesCmb(_connStr);
+		}
+
+		private void testConnection_Click(object sender, System.EventArgs e)
+		{
+			try
+			{
+				SetConnectionString();
+				using(SqlConnection conn = new SqlConnection(ConnectionString))
+				{
+					conn.Open();
+					if(conn.State == ConnectionState.Open)
+					{
+						connResult.Text="Connection Succeded";
+						return;
+					}
+				}
+			}
+			catch(Exception)
+			{
+				
+			}
+
+			connResult.Text="Connection Failed";
 		}
 
 		/// <summary>
@@ -115,17 +477,22 @@ namespace Subtext.Web.Controls
 		[DefaultValue("")]
 		[Category("Behavior")]
 		[Description("The Connection String.")]
-		public string ConnectionString
+		public String ConnectionString
 		{
 			get
 			{
 				if(UseWebConfigCheckBox != null && UseWebConfigCheckBox.Checked)
 					return System.Configuration.ConfigurationSettings.AppSettings["ConnectionString"];
-
-				return ConnectionStringTextBox.Text;
+				if(CheckSQLDMO()) 
+				{
+					return _connStr.ToString();
+				}
+				else
+					return ConnectionStringTextBox.Text;
 			}
 			set
 			{
+				_connStr = Subtext.Scripting.ConnectionString.Parse(value);
 				ConnectionStringTextBox.Text = value;
 			}
 		}
@@ -174,6 +541,8 @@ namespace Subtext.Web.Controls
 			}
 		}
 
+		#region Accessors for Controls in page
+
 		TextBox ConnectionStringTextBox
 		{
 			get
@@ -210,5 +579,8 @@ namespace Subtext.Web.Controls
 				return FindControl(TitleControlId) as LiteralControl;
 			}
 		}
+		#endregion // Accessors for Controls in page
+
+
 	}
 }
