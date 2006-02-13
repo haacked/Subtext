6f69a0727e31a486aead928614bd86d8a909847a
haacked <haacked@483c6c4e-18e1-11de-b208-e5a9466b8568>
2006-02-13 01:08:00 +0000

    Updated the SharpZipLib with the latest version from (http://www.icsharpcode.net/OpenSource/SharpZipLib/Default.aspx).

---
+++ SubtextSystem/SubtextSolution/Subtext.Framework/ContentCache.cs
@@ -0,0 +1,105 @@
+using System;
+using System.Collections;
+using System.Globalization;
+using System.Threading;
+using System.Web;
+using System.Web.Caching;
+
+namespace Subtext.Framework
+{
+	/// <summary>
+	/// This is a replacement for the <see cref="Cache"/> object. 
+	/// Use this when caching content.  This ensures content is 
+	/// cached according to key and Locale.
+	/// </summary>
+	public class ContentCache : IEnumerable
+	{
+		Cache cache;
+
+		/// <summary>
+		/// Instantiates the specified content cache from the specific <see cref="HttpContext"/>. 
+		/// At some point, we might consider replacing HttpContext with a type we can extend.
+		/// </summary>
+		/// <returns></returns>
+		public static ContentCache Instantiate()
+		{
+			//Check per-request cache.
+			ContentCache cache = HttpContext.Current.Items["ContentCache"] as ContentCache;
+			if(cache != null)
+				return cache;
+
+			cache = new ContentCache(HttpContext.Current.Cache);
+			//Per-Request Cache.
+			HttpContext.Current.Items["ContentCache"] = cache;
+			return cache;
+		}
+
+		private ContentCache() {}
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="ContentCache"/> class. 
+		/// The specified <see cref="Cache"/> instance is wrapped by this instance.
+		/// </summary>
+		/// <param name="cache">The cache.</param>
+		private ContentCache(Cache cache)
+		{
+			this.cache = cache;
+		}
+
+		//Returns a language aware cache key.
+		private string GetCacheKey(string key)
+		{
+			return key + ":" + Thread.CurrentThread.CurrentCulture.LCID.ToString(CultureInfo.InvariantCulture);
+		}
+
+		/// <summary>
+		/// Gets or sets the <see cref="Object"/> with the specified key.
+		/// </summary>
+		/// <value></value>
+		public object this[string key]
+		{
+			get
+			{
+				return this.cache[GetCacheKey(key)];
+			}
+			set
+			{
+				this.cache.Insert(GetCacheKey(key), value);
+			}
+		}
+
+		/// <summary>
+		/// Inserts the specified object to the <see cref="System.Web.Caching.Cache"/> object 
+		/// with a cache key to reference its location and using default values provided by 
+		/// the <see cref="System.Web.Caching.CacheItemPriority"/> enumeration.
+		/// </summary>
+		/// <param name="key">The key.</param>
+		/// <param name="value">The value.</param>
+		public void Insert(string key, object value)
+		{
+			this.cache.Insert(GetCacheKey(key), value);
+		}
+
+		/// <summary>
+		/// Retrieves the specified item from the <see cref="System.Web.Caching.Cache"/>.
+		/// </summary>
+		/// <param name="key"></param>
+		/// <returns></returns>
+		public object Get(string key)
+		{
+			return this.cache.Get(GetCacheKey(key));
+		}
+
+		/// <summary>
+		/// Returns an enumerator that can iterate through a collection.
+		/// </summary>
+		/// <returns>
+		/// An <see cref="T:System.Collections.IEnumerator"/>
+		/// that can be used to iterate through the collection.
+		/// </returns>
+		public IEnumerator GetEnumerator()
+		{
+			return this.cache.GetEnumerator();
+		}
+	}
+}
