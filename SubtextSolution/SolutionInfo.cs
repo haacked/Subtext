92eeed37a687665093579ee07ed8e4c1cbd0da40
simo <simo@483c6c4e-18e1-11de-b208-e5a9466b8568>
2006-04-10 14:29:46 +0000

    Moved AssemblyVersionAttribute from SolutionInfo.cs to a new VersionInfo.Designer.cs file. I also linked these 2 files from all projects in order to have all versions in sync.
    (The file is named VersionInfo.Designer.cs because VersionInfo.cs already exists in another project)

--- SubtextSolution/SolutionInfo.cs
+++ SubtextSolution/SolutionInfo.cs
@@ -19,7 +19,7 @@ using System.Reflection;
 [assembly: AssemblyCompany("Subtext")]
 [assembly: AssemblyProduct("Subtext Blog Engine")]
 [assembly: AssemblyCopyright("Copyright (c) 2005 - 2006 Subtext Project Team - BSD License")]
-[assembly: AssemblyVersion("1.0.4.0")]
+/* [assembly: AssemblyVersion("1.0.4.0")] moved to VersionInfo.cs for automatic generation */
 /* Removed [AssemblyFileVersion] since it defaults to AssemblyVersion */
 /* Human Readable version. */
 [assembly: AssemblyInformationalVersion("1.0.5.0")]
