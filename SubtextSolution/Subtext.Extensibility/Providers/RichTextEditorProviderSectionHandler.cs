aca589981394ff2b0408d30395e6ea055670e564
simo <simo@483c6c4e-18e1-11de-b208-e5a9466b8568>
2006-04-14 16:00:39 +0000

    Added stub implementation of a RichTextEditorProvider, the provider to manage rich text editors

---
+++ SubtextSolution/Subtext.Extensibility/Providers/RichTextEditorProviderSectionHandler.cs
@@ -0,0 +1,37 @@
+#region Disclaimer/Info
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Subtext WebLog
+// 
+// Subtext is an open source weblog system that is a fork of the .TEXT
+// weblog system.
+//
+// For updated news and information please visit http://subtextproject.com/
+// Subtext is hosted at SourceForge at http://sourceforge.net/projects/subtext
+// The development mailing list is at subtext-devs@lists.sourceforge.net 
+//
+// This project is licensed under the BSD license.  See the License.txt file for more information.
+///////////////////////////////////////////////////////////////////////////////////////////////////
+#endregion
+
+using System;
+
+namespace Subtext.Extensibility.Providers
+{
+	/// <summary>
+	/// Configuration section handler for the <see cref="RichTextEditorProvider"/>.  
+	/// Most of the implementation is in the base <see cref="ProviderSectionHandler"/> 
+	/// class.  This merely follows the factory pattern in providing a method 
+	/// to create a proper <see cref="ProviderConfiguration"/> instance.
+	/// </summary>
+	public class RichTextEditorProviderSectionHandler: ProviderSectionHandler
+	{
+		/// <summary>
+		/// Creates an <see cref="RichTextEditorProviderConfiguration"/> instance which 
+		/// is populated by this section handler.
+		/// </summary>
+		public override ProviderConfiguration CreateProviderConfigurationInstance()
+		{
+			return new RichTextEditorProviderConfiguration();
+		}
+	}
+}
