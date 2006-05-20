0eaaf75b18b3e824a63057f5bed1062736551035
Haacked <haacked@gmail.com>
2006-05-20 20:15:03 +0000

    Forgot to add these files related to refactoring some code into HTTP Modules.

---
+++ SubtextSolution/Subtext.Framework/Web/HttpModules/BlogRequestModule.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Globalization;
+using System.Web;
+using Subtext.Framework.Configuration;
+using Subtext.Framework.Format;
+using Subtext.Framework.Text;
+using Subtext.Framework.Web.HttpModules;
+
+namespace Subtext.Web.HttpModules
+{
+	/// <summary>
+	/// Examines incoming http requests and adds Subtext specific as well as blog 
+	/// specific information to the context.
+	/// </summary>
+	public class BlogRequestModule : IHttpModule
+	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="BlogRequestModule"/> class.
+		/// </summary>
+		public BlogRequestModule()
+		{
+		}
+
+		/// <summary>
+		/// Initializes a module and prepares it to handle
+		/// requests.
+		/// </summary>
+		/// <param name="context">An <see cref="T:System.Web.HttpApplication"/> that provides access to the methods, properties, and events common to all application objects within an ASP.NET application</param>
+		public void Init(HttpApplication context)
+		{
+			context.BeginRequest += new EventHandler(MapUrlToBlogStatus);
+		}
+
+		/// <summary>
+		/// Disposes of the resources (other than memory) used by the
+		/// module that implements <see langword="IHttpModule."/>
+		/// </summary>
+		public void Dispose()
+		{
+			//Do Nothing.
+		}
+
+		/// <summary>
+		/// Maps the incoming URL to the corresponding blog. If no blog matches, then 
+		/// makes sure any host application settings are still set.
+		/// </summary>
+		/// <param name="sender">The sender.</param>
+		/// <param name="e">The <see cref="System.EventArgs"/> instance containing the event data.</param>
+		private void MapUrlToBlogStatus(object sender, EventArgs e)
+		{
+			string subfolder = UrlFormats.GetBlogSubfolderFromRequest(HttpContext.Current.Request.RawUrl, HttpContext.Current.Request.ApplicationPath);
+			if(!Config.IsValidSubfolderName(subfolder))
+				subfolder = string.Empty;
+			
+			HttpContext.Current.Items["Subtext__CurrentRequest"] = new BlogRequest(Host, subfolder);
+		}
+		
+		/// <summary>
+		/// Gets the current host, stripping off the initial "www." if 
+		/// found.
+		/// </summary>
+		/// <returns></returns>
+		protected string Host
+		{
+			get
+			{
+				string host = HttpContext.Current.Request.Url.Host;
+				if(!HttpContext.Current.Request.Url.IsDefaultPort)
+				{
+					host  += ":" + HttpContext.Current.Request.Url.Port.ToString(CultureInfo.InvariantCulture);
+				}
+
+				if(StringHelper.StartsWith(host, "www.", ComparisonType.CaseInsensitive))
+				{
+					host = host.Substring(4);
+				}
+				return host;
+			}
+		}
+	}
+}
---
+++ SubtextSolution/Subtext.Framework/Web/HttpModules/InstallationCheckModule.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Web;
+using Subtext.Extensibility.Providers;
+using Subtext.Framework;
+
+namespace Subtext.Web.HttpModules
+{
+	/// <summary>
+	/// Maps an incoming URL to a blog.
+	/// </summary>
+	public class InstallationCheckModule : IHttpModule
+	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="InstallationCheckModule"/> class.
+		/// </summary>
+		public InstallationCheckModule()
+		{
+		}
+
+		/// <summary>
+		/// Initializes a module and prepares it to handle
+		/// requests.
+		/// </summary>
+		/// <param name="context">An <see cref="T:System.Web.HttpApplication"/> that provides access to the methods, properties, 
+		/// and events common to all application objects within an ASP.NET application</param>
+		public void Init(System.Web.HttpApplication context)
+		{
+			context.BeginRequest += new EventHandler(CheckInstallationStatus);
+		}
+
+		/// <summary>
+		/// Disposes of the resources (other than memory) used by the
+		/// module that implements <see langword="IHttpModule."/>
+		/// </summary>
+		public void Dispose()
+		{
+			//Do nothing.
+		}
+
+		/// <summary>
+		/// Checks the installation status and redirects if necessary.
+		/// </summary>
+		/// <param name="sender">The sender.</param>
+		/// <param name="e">The <see cref="System.EventArgs"/> instance containing the event data.</param>
+		public void CheckInstallationStatus(object sender, EventArgs e)
+		{
+			// Want to redirect to install if installation is required, 
+			// or if we're missing a HostInfo record.
+			if((InstallationManager.IsInstallationActionRequired(VersionInfo.FrameworkVersion) || InstallationManager.HostInfoRecordNeeded))
+			{
+				InstallationState state = InstallationManager.GetCurrentInstallationState(VersionInfo.FrameworkVersion);
+				if(state == InstallationState.NeedsInstallation && !InstallationManager.IsInHostAdminDirectory && !InstallationManager.IsInInstallDirectory)
+				{
+					HttpContext.Current.Response.Redirect("~/Install/", true);
+					return;
+				}
+
+				if(state == InstallationState.NeedsUpgrade || state == InstallationState.NeedsRepair)
+				{
+					if(!InstallationManager.IsInUpgradeDirectory && !InstallationManager.IsOnLoginPage && !InstallationManager.IsInSystemMessageDirectory)
+					{
+						HttpContext.Current.Response.Redirect("~/SystemMessages/UpgradeInProgress.aspx", true);
+						return;
+					}
+				}
+			}
+		}
+	}
+}
