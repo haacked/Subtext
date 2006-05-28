6240bd895ae8e37c0cef056dd35b8e957d174e37
Haacked <haacked@gmail.com>
2006-05-28 19:02:26 +0000

    Forgot to add some files and broke the build. Mea Culpa!

---
+++ SubtextSolution/UnitTests.Subtext/Extensibility/EmailProviderInstantiationTests.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections.Specialized;
+using MbUnit.Framework;
+using Subtext.Extensibility.Providers;
+using Subtext.Framework.Email;
+
+namespace UnitTests.Subtext.Extensibility
+{
+	/// <summary>
+	/// Summary description for ProviderInstantiationTests.
+	/// </summary>
+	[TypeFixture(typeof(EmailProvider))]
+	[ProviderFactory(typeof(EmailProviderFactory), typeof(EmailProvider))]
+	public class EmailProviderInstantiationTests
+	{
+		[Test]
+		public void CanInstantiateAndInitializeEmailProvider(EmailProvider provider)
+		{
+			Assert.IsNotNull(provider, "Well the provider should not be null.");
+			NameValueCollection configValue = new NameValueCollection();
+			configValue["adminEmail"] = "admin@example.com";
+			configValue["smtpServer"] = "smtp.example.com";
+			configValue["password"] = "abracadabra";;
+			configValue["username"] = "haacked";
+			provider.Initialize("providerTest", configValue);
+			
+			Assert.AreEqual("admin@example.com", provider.AdminEmail, "Did not initialize the admin email properly.");
+			Assert.AreEqual("smtp.example.com", provider.SmtpServer, "Did not initialize the SMTP server properly.");
+			Assert.AreEqual("abracadabra", provider.Password, "Did not initialize the password properly.");
+			Assert.AreEqual("haacked", provider.UserName, "Did not initialize the username properly.");
+		}
+		
+		[Test]
+		[ExpectedException(typeof(ArgumentNullException))]
+		public void GetSettingValueThrowsArgumentNullExceptionForNullSettingKey(EmailProvider provider)
+		{
+			provider.GetSettingValue(null, new NameValueCollection());
+		}
+		
+		[Test]
+		[ExpectedException(typeof(ArgumentNullException))]
+		public void GetSettingValueThrowsArgumentNullExceptionForNullConfigValues(EmailProvider provider)
+		{
+			provider.GetSettingValue("Section", null);
+		}
+	}
+	
+	internal class EmailProviderFactory
+	{
+		[Factory]
+		public EmailProvider DotNetOpenMailProvider
+		{
+			get
+			{
+				return new DotNetOpenMailProvider();
+			}
+		}
+		
+		[Factory]
+		public EmailProvider SystemMailProvider
+		{
+			get
+			{
+				return new SystemMailProvider();
+			}
+		}
+	}
+}
---
+++ SubtextSolution/UnitTests.Subtext/Extensibility/ProviderBaseTests.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Xml;
+using MbUnit.Framework;
+using Subtext.Extensibility.Exceptions;
+using Subtext.Extensibility.Providers;
+
+namespace UnitTests.Subtext.Extensibility
+{
+	/// <summary>
+	/// Summary description for ProviderBaseTests.
+	/// </summary>
+	[TestFixture]
+	public class ProviderBaseTests
+	{
+		[Test]
+		[ExpectedException(typeof(ProviderInstantiationException))]
+		public void ThrowsProviderInstantiationExceptionWhenInvalidTypeSpecified()
+		{
+			ProviderInfo providerInfo = UnitTestHelper.CreateProviderInfoInstance("test", "test");
+			ProviderBase.Instance("Section", providerInfo);
+		}
+		
+		[Test]
+		[ExpectedException(typeof(ArgumentNullException))]
+		public void NullSectionNameThrowsException()
+		{
+			ProviderInfo providerInfo = UnitTestHelper.CreateProviderInfoInstance("test", "test");
+			ProviderBase.Instance(null, providerInfo);
+		}
+		
+		[Test]
+		[ExpectedException(typeof(ArgumentNullException))]
+		public void NullProviderInfoThrowsException()
+		{
+			ProviderBase.Instance("Section", null);
+		}
+	}
+}
