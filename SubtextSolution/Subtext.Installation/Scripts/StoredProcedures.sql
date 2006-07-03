80664f1bafe9150c6ff3c131ecd07b53dce49a3b
Haacked <haacked@gmail.com>
2006-07-03 05:25:54 +0000

    [1514406] Subtext_GetEntry_PreviousNext did not have permissions.

--- SubtextSolution/Subtext.Installation/Scripts/StoredProcedures.sql
+++ SubtextSolution/Subtext.Installation/Scripts/StoredProcedures.sql
@@ -4379,7 +4379,7 @@ GO
 SET ANSI_NULLS ON 
 GO
 
-CREATE Proc [<dbUser,varchar,dbo>].Subtext_GetEntry_PreviousNext
+CREATE Proc [<dbUser,varchar,dbo>].[Subtext_GetEntry_PreviousNext]
 (
  @ID int,
  @PostType int = 1,
@@ -4403,6 +4403,10 @@ SET QUOTED_IDENTIFIER OFF
 GO
 SET ANSI_NULLS ON 
 GO
+
+GRANT  EXECUTE  ON [<dbUser,varchar,dbo>].[Subtext_GetEntry_PreviousNext]  TO [public]
+GO
+
 
 /*Get Related Links (called from RelatedLinks.ascx) - GY*/
 SET QUOTED_IDENTIFIER OFF 
