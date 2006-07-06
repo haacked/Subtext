18d08b4f5df600abc6f8a303d8be65f2078015f5
simo <simo@483c6c4e-18e1-11de-b208-e5a9466b8568>
2006-07-06 19:44:58 +0000

    Changed DB creation script to use all the available scripts

--- Utilities/Testing/InstallDatabaseLocally.bat
+++ Utilities/Testing/InstallDatabaseLocally.bat
@@ -6,6 +6,8 @@ SET DBNAME=%1%
 IF "%DBNAME%" == "" SET DBNAME=SubtextData
 
 OSQL -E -d %DBNAME% -i ..\..\SubTextSolution\Subtext.Installation\Scripts\Installation.01.00.00.sql
+OSQL -E -d %DBNAME% -i ..\..\SubTextSolution\Subtext.Installation\Scripts\Installation.01.05.00.sql
+OSQL -E -d %DBNAME% -i ..\..\SubTextSolution\Subtext.Installation\Scripts\Installation.01.09.00.sql
 OSQL -E -d %DBNAME% -i ..\..\SubTextSolution\Subtext.Installation\Scripts\StoredProcedures.sql
 
 
