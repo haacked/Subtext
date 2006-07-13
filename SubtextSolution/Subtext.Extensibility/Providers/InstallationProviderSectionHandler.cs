e3d0245f92798d708150638fc90c628b58828f7a
simo <simo@483c6c4e-18e1-11de-b208-e5a9466b8568>
2006-07-13 09:30:07 +0000

    Refactored InstallationProvider to use System.Configuration.Provider.ProviderBase instead of our custom provider implementation. Created a ProviderConfigurationHelper class used to load replace configuration placeholder with real configuration value (used for connectionString) Also fixed SqlInstallationProvider to avoid the same error had with ImportProvider which didn't call the base.Initialize method.

--- SubtextSolution/Subtext.Extensibility/Providers/InstallationProviderSectionHandler.cs
+++ SubtextSolution/Subtext.Extensibility/Providers/InstallationProviderSectionHandler.cs
@@ -14,24 +14,23 @@
 #endregion
 
 using System;
+using System.Configuration;
 
 namespace Subtext.Extensibility.Providers
 {
-	/// <summary>
-	/// Configuration section handler for the <see cref="InstallationProvider"/>.  
-	/// Most of the implementation is in the base <see cref="ProviderSectionHandler"/> 
-	/// class.  This merely follows the factory pattern in providing a method 
-	/// to create a proper <see cref="ProviderConfiguration"/> instance.
-	/// </summary>
-	public class InstallationProviderSectionHandler : ProviderSectionHandler
+    public class InstallationProviderSectionHandler : ConfigurationSection
 	{
-		/// <summary>
-		/// Creates an <see cref="InstallationProviderConfiguration"/> instance which 
-		/// is populated by this section handler.
-		/// </summary>
-		public override ProviderConfiguration CreateProviderConfigurationInstance()
-		{
-			return new InstallationProviderConfiguration();
-		}
+        [ConfigurationProperty("providers")]
+        public ProviderSettingsCollection Providers
+        {
+            get { return (ProviderSettingsCollection)base["providers"]; }
+        }
+
+        [ConfigurationProperty("defaultProvider")]
+        public string DefaultProvider
+        {
+            get { return (string)base["defaultProvider"]; }
+            set { base["defaultProvider"] = value; }
+        }
 	}
 }
