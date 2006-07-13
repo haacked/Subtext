31b95d0b5810281591e2a9782091f23211228992
Haacked <haacked@gmail.com>
2006-07-13 03:04:23 +0000

    Updated the WHITE skin. Also added a file I forgot to add.

---
+++ SubtextSolution/Subtext.Web/UI/Controls/PostComment.Designer.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace Subtext.Web.UI.Controls
+{
+	public partial class PostComment
+	{
+		protected System.Web.UI.WebControls.TextBox tbTitle;
+		protected System.Web.UI.WebControls.TextBox tbName;
+		protected System.Web.UI.WebControls.TextBox tbUrl;
+		protected System.Web.UI.WebControls.TextBox tbComment;
+		protected System.Web.UI.WebControls.TextBox tbEmail;
+		protected System.Web.UI.WebControls.Button btnSubmit;
+		protected Subtext.Web.Controls.CompliantButton btnCompliantSubmit;
+		protected System.Web.UI.WebControls.Label Message;
+		protected System.Web.UI.WebControls.CheckBox chkRemember;
+		protected SubtextCoComment coComment;
+	}
+}
