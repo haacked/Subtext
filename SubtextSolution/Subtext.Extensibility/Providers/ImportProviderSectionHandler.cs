56963c313c45836f759dd7f006509c2904a47dfe
simo <simo@483c6c4e-18e1-11de-b208-e5a9466b8568>
2006-07-13 06:26:04 +0000

    Refactored ImportProvider to use System.Configuration.Provider instead of our implementation of ProviderBase Had to change a lot of things also in the usage because we never use the DefaultProvider but we decide which provider to use at runtime, so different from all other providers. This probably breaks some unit tests, and probably also the import process. But I've to go to work now (8:20am). I'll fix it later when I arrive at my office. Sorry for any problem that may cause.

--- SubtextSolution/Subtext.Extensibility/Providers/ImportProviderSectionHandler.cs
+++ SubtextSolution/Subtext.Extensibility/Providers/ImportProviderSectionHandler.cs
@@ -14,24 +14,23 @@
 #endregion
 
 using System;
+using System.Configuration;
 
 namespace Subtext.Extensibility.Providers
 {
-	/// <summary>
-	/// Configuration section handler for the <see cref="ImportProvider"/>.  
-	/// Most of the implementation is in the base <see cref="ProviderSectionHandler"/> 
-	/// class.  This merely follows the factory pattern in providing a method 
-	/// to create a proper <see cref="ProviderConfiguration"/> instance.
-	/// </summary>
-	public class ImportProviderSectionHandler : ProviderSectionHandler
+    public class ImportProviderSectionHandler : ConfigurationSection
 	{
-		/// <summary>
-		/// Creates an <see cref="InstallationProviderConfiguration"/> instance which 
-		/// is populated by this section handler.
-		/// </summary>
-		public override ProviderConfiguration CreateProviderConfigurationInstance()
-		{
-			return new ImportProviderConfiguration();
-		}
+        [ConfigurationProperty("providers")]
+        public ProviderSettingsCollection Providers
+        {
+            get { return (ProviderSettingsCollection)base["providers"]; }
+        }
+
+        [ConfigurationProperty("defaultProvider")]
+        public string DefaultProvider
+        {
+            get { return (string)base["defaultProvider"]; }
+            set { base["defaultProvider"] = value; }
+        }
 	}
 }
