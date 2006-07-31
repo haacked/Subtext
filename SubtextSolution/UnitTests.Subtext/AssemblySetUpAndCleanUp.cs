035e8a77b5996ab86d02c0d9755850e7a281e593
Haacked <haacked@gmail.com>
2006-07-31 03:19:12 +0000

    Added an Assembly level setup method for our unit tests using the AssemblyCleanupAttribute class.
    This allows us to have a unit test step that runs just once. Right now, it recreates the stored procedures.

---
+++ SubtextSolution/UnitTests.Subtext/AssemblySetUpAndCleanUp.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Configuration;
+using System.Data.SqlClient;
+using MbUnit.Framework;
+using Subtext.Installation;
+
+namespace UnitTests.Subtext
+{
+	public static class AssemblySetUpAndCleanUp
+	{
+		[SetUp]
+		public static void SetUp()
+		{
+			using (	SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["subtextData"].ConnectionString))
+			{
+				connection.Open();
+				using (SqlTransaction transaction = connection.BeginTransaction())
+				{
+					ScriptHelper.ExecuteScript("StoredProcedures.sql", transaction);
+					transaction.Commit();
+				}
+			}
+		}
+		
+		[TearDown]
+		public static void TearDown()
+		{
+			//Not sure we need anything here yet.
+		}
+	}
+}
