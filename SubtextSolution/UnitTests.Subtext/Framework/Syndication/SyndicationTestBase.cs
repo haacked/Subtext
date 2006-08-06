05c5db7ebef660ea3a3cf121357d213a0d1823c9
haacked <haacked@483c6c4e-18e1-11de-b208-e5a9466b8568>
2006-08-06 06:58:09 +0000

    Added SyndicationTestBase.cs.

---
+++ SubtextSolution/UnitTests.Subtext/Framework/Syndication/SyndicationTestBase.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace UnitTests.Subtext.Framework.Syndication
+{
+	public class SyndicationTestBase
+	{
+		protected string indent()
+		{
+			return "    ";
+		}
+
+		protected string indent(int count)
+		{
+			string returnval = string.Empty;
+			for (int i = 0; i < count; i++)
+			{
+				returnval += indent();
+			}
+			return returnval;
+		}
+	}
+}
