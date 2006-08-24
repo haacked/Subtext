053818897476781378baf4b2747dcf6c02bd527e
Haacked <haacked@gmail.com>
2006-08-24 07:57:52 +0000

    Merged revision 1693 from Release branch Release1.9

---
+++ SubtextSolution/Subtext.Web/Install/InstallTemplate.Master.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Data;
+using System.Configuration;
+using System.Collections;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI.HtmlControls;
+
+namespace Subtext.Web.Install
+{
+	public partial class InstallTemplate : System.Web.UI.MasterPage
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
