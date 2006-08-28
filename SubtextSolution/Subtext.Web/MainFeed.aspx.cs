ffd9efbb2d071e498518fa9706e119c0d1d83095
haacked <haacked@483c6c4e-18e1-11de-b208-e5a9466b8568>
2006-08-28 02:26:47 +0000

    Merged revision 1729 into trunk.

--- SubtextSolution/Subtext.Web/MainFeed.aspx.cs
+++ SubtextSolution/Subtext.Web/MainFeed.aspx.cs
@@ -62,97 +62,90 @@ namespace Subtext.Web
 			{
 				appPath += "/";
 			}
-			try
+			
+			StringWriter sw = new StringWriter();
+			XmlTextWriter writer = new XmlTextWriter(sw);
+
+			//RSS ROOT
+			writer.WriteStartElement("rss");
+			writer.WriteAttributeString("version","2.0");
+			writer.WriteAttributeString("xmlns:dc","http://purl.org/dc/elements/1.1/");
+			writer.WriteAttributeString("xmlns:trackback","http://madskills.com/public/xml/rss/module/trackback/");
+			writer.WriteAttributeString("xmlns:wfw","http://wellformedweb.org/CommentAPI/");
+			writer.WriteAttributeString("xmlns:slash","http://purl.org/rss/1.0/modules/slash/");
+
+			//Channel
+			writer.WriteStartElement("channel");
+			//Channel Description
+			writer.WriteElementString("title", ConfigurationManager.AppSettings["AggregateTitle"]);
+			writer.WriteElementString("link",Context.Request.Url.ToString());
+			writer.WriteElementString("description", ConfigurationManager.AppSettings["AggregateDescription"]);
+			
+			//CHANGE: FrameworkVersion used insted of Version which is not exist.
+			writer.WriteElementString("generator",Subtext.Framework.VersionInfo.FrameworkVersion.ToString());
+
+			int count = dt.Rows.Count;
+			int servertz = Config.Settings.ServerTimeZone;
+			string baseUrl = "http://{0}" + appPath + "{1}/";
+
+			bool useAggBugs = Config.Settings.Tracking.EnableAggBugs;
+
+			for(int i = 0; i< count; i++)
 			{
-				StringWriter sw = new StringWriter();
-				XmlTextWriter writer = new XmlTextWriter(sw);
+				DataRow dr = dt.Rows[i];
 
-				//RSS ROOT
-				writer.WriteStartElement("rss");
-				writer.WriteAttributeString("version","2.0");
-				writer.WriteAttributeString("xmlns:dc","http://purl.org/dc/elements/1.1/");
-				writer.WriteAttributeString("xmlns:trackback","http://madskills.com/public/xml/rss/module/trackback/");
-				writer.WriteAttributeString("xmlns:wfw","http://wellformedweb.org/CommentAPI/");
-				writer.WriteAttributeString("xmlns:slash","http://purl.org/rss/1.0/modules/slash/");
+				writer.WriteStartElement("item");
+				writer.WriteElementString("title", (string)dr["Title"]);
 
-				//Channel
-				writer.WriteStartElement("channel");
-				//Channel Description
-                writer.WriteElementString("title", ConfigurationManager.AppSettings["AggregateTitle"]);
-				writer.WriteElementString("link",Context.Request.Url.ToString());
-                writer.WriteElementString("description", ConfigurationManager.AppSettings["AggregateDescription"]);
+				string baselink = string.Format(baseUrl, dr["Host"], dr["Application"]);
+				string link = string.Format(CultureInfo.InvariantCulture, baselink + "archive/{0:yyyy/MM/dd}/{1}.aspx", ((DateTime)dr["DateAdded"]), dr["EntryName"]);
+				writer.WriteElementString("link",link);
+
+				DateTime time = (DateTime)dr["DateAdded"];
+				int tz = (int)dr["TimeZone"];
+				int offset = (servertz - tz);
 				
-				//CHANGE: FrameworkVersion used insted of Version which is not exist.
-				writer.WriteElementString("generator",Subtext.Framework.VersionInfo.FrameworkVersion.ToString());
+				
+				writer.WriteElementString("pubDate",(time.AddHours(offset)).ToUniversalTime().ToString("r"));
+				//writer.WriteElementString("guid",link);
+				writer.WriteStartElement("guid");
+				writer.WriteAttributeString("isPermaLink","true");
+				writer.WriteString(link);
+				writer.WriteEndElement();
 
-				int count = dt.Rows.Count;
-				int servertz = Config.Settings.ServerTimeZone;
-				string baseUrl = "http://{0}" + appPath + "{1}/";
-
-				bool useAggBugs = Config.Settings.Tracking.EnableAggBugs;
-
-				for(int i = 0; i< count; i++)
+				writer.WriteElementString("wfw:comment",string.Format(baselink + "comments/{0}.aspx",dr["ID"]));
+				writer.WriteElementString("wfw:commentRss", string.Format(baselink + "comments/commentRss/{0}.aspx",dr["ID"]));
+				writer.WriteElementString("comments",link + "#comment");
+				int feedbackCount = 0;
+				if(dr["FeedBackCount"] != DBNull.Value)
 				{
-					DataRow dr = dt.Rows[i];
-
-					writer.WriteStartElement("item");
-					writer.WriteElementString("title", (string)dr["Title"]);
-
-					string baselink = string.Format(baseUrl, dr["Host"], dr["Application"]);
-					string link = string.Format(CultureInfo.InvariantCulture, baselink + "archive/{0:yyyy/MM/dd}/{1}.aspx", ((DateTime)dr["DateAdded"]), dr["EntryName"]);
-					writer.WriteElementString("link",link);
-
-					DateTime time = (DateTime)dr["DateAdded"];
-					int tz = (int)dr["TimeZone"];
-					int offset = (servertz - tz);
-					
-					
-					writer.WriteElementString("pubDate",(time.AddHours(offset)).ToUniversalTime().ToString("r"));
-					//writer.WriteElementString("guid",link);
-					writer.WriteStartElement("guid");
-					writer.WriteAttributeString("isPermaLink","true");
-					writer.WriteString(link);
-					writer.WriteEndElement();
-
-					writer.WriteElementString("wfw:comment",string.Format(baselink + "comments/{0}.aspx",dr["ID"]));
-					writer.WriteElementString("wfw:commentRss", string.Format(baselink + "comments/commentRss/{0}.aspx",dr["ID"]));
-					writer.WriteElementString("comments",link + "#comment");
-					int feedbackCount = 0;
-					if(dr["FeedBackCount"] != DBNull.Value)
-					{
-						feedbackCount = (int)dr["FeedBackCount"];
-					}
-					writer.WriteElementString("slash:comments", feedbackCount.ToString(CultureInfo.InvariantCulture));
-					writer.WriteElementString("trackback:ping",string.Format(baselink + "services/trackbacks/{0}.aspx",dr["ID"]));
-
-
-					writer.WriteStartElement("source");
-					writer.WriteAttributeString("url",baselink + "rss.aspx");
-					writer.WriteString((string)dr["BlogTitle"]);
-					writer.WriteEndElement();
-
-					string desc = (string)dr["Description"];
-
-					string aggText = useAggBugs ? Subtext.Framework.Tracking.TrackingUrls.AggBugImage(string.Format(baselink + "aggbug/{0}.aspx",dr["ID"])) : string.Empty;
-
-					writer.WriteElementString("description", string.Format("{0}{1}", desc, aggText));
-					writer.WriteElementString("dc:creator",(string)dr["Author"]);	
-					writer.WriteEndElement();
-				
+					feedbackCount = (int)dr["FeedBackCount"];
 				}
-				writer.WriteEndElement();
-				
-				writer.WriteEndElement();
-				writer.Flush();
-				writer.Close();
-				sw.Close();
-				return sw.ToString();
+				writer.WriteElementString("slash:comments", feedbackCount.ToString(CultureInfo.InvariantCulture));
+				writer.WriteElementString("trackback:ping",string.Format(baselink + "services/trackbacks/{0}.aspx",dr["ID"]));
 
+
+				writer.WriteStartElement("source");
+				writer.WriteAttributeString("url",baselink + "rss.aspx");
+				writer.WriteString((string)dr["BlogTitle"]);
+				writer.WriteEndElement();
+
+				string desc = (string)dr["Description"];
+
+				string aggText = useAggBugs ? Subtext.Framework.Tracking.TrackingUrls.AggBugImage(string.Format(baselink + "aggbug/{0}.aspx",dr["ID"])) : string.Empty;
+
+				writer.WriteElementString("description", string.Format("{0}{1}", desc, aggText));
+				writer.WriteElementString("dc:creator",(string)dr["Author"]);	
+				writer.WriteEndElement();
+			
 			}
-			catch(Exception e)
-			{
-				throw;
-			}
+			writer.WriteEndElement();
+			
+			writer.WriteEndElement();
+			writer.Flush();
+			writer.Close();
+			sw.Close();
+			return sw.ToString();
 		}
 
 		/// <summary>
