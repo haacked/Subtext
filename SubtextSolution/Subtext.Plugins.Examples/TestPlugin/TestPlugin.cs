ddce97f9ab7e809642849da18b8782d7f38b7bd2
simo <simo@483c6c4e-18e1-11de-b208-e5a9466b8568>
2006-11-08 15:19:08 +0000

    Implemented 3 test event handlers:  - PreEntryUpdate  - PostEntryUpdate  - PreRenderEntry

--- SubtextSolution/Subtext.Plugins.Examples/TestPlugin/TestPlugin.cs
+++ SubtextSolution/Subtext.Plugins.Examples/TestPlugin/TestPlugin.cs
@@ -21,10 +21,35 @@ namespace Subtext.Plugins.Examples.TestPlugin
 
 		public void Init(STApplication sta)
 		{
+			sta.PreEntryUpdate += new EntryEventHandler(sta_PreEntryUpdate);
+			sta.PostEntryUpdate += new EntryEventHandler(sta_PostEntryUpdate);
+			sta.PreRenderEntry += new EntryEventHandler(sta_PreRenderEntry);
 		}
 
-		void Current_PreEntryUpdate(object sender, EventArgs e)
+		void sta_PreRenderEntry(Subtext.Framework.Components.Entry entry, STEventArgs e)
 		{
+			entry.Body += "<br><hr> <b>TestPlugin</b>: Rendered at date: " + DateTime.Now.ToShortDateString() + " " + DateTime.Now.ToShortTimeString();
+		}
+
+		void sta_PostEntryUpdate(Subtext.Framework.Components.Entry entry, STEventArgs e)
+		{
+			string url = entry.FullyQualifiedUrl.ToString();
+			return;
+		}
+
+		void sta_PreEntryUpdate(Subtext.Framework.Components.Entry entry, STEventArgs e)
+		{
+			switch (e.State)
+			{
+				case ObjectState.Create:
+					entry.Body += "<br><hr> <b>TestPlugin</b>: Created at date: " + DateTime.Now.ToShortDateString() + " " + DateTime.Now.ToShortTimeString();
+					break;
+				case ObjectState.Update:
+					entry.Body += "<br><hr> <b>TestPlugin</b>: Updated at date: " + DateTime.Now.ToShortDateString() + " " + DateTime.Now.ToShortTimeString();
+					break;
+				default:
+					break;
+			}
 		}
 
 		#endregion
