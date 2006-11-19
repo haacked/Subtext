07a6a2427c8eaf1cf2a7894bcd9921f5d1f31b24
simo <simo@483c6c4e-18e1-11de-b208-e5a9466b8568>
2006-11-19 14:24:31 +0000

    Remeved CommunityCredits web reference from subtext.web project

--- SubtextSolution/Subtext.Web/Properties/Settings.Designer.cs
+++ SubtextSolution/Subtext.Web/Properties/Settings.Designer.cs
@@ -22,15 +22,5 @@ namespace Subtext.Web.Properties {
                 return defaultInstance;
             }
         }
-        
-        [global::System.Configuration.ApplicationScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.WebServiceUrl)]
-        [global::System.Configuration.DefaultSettingValueAttribute("http://www.community-credit.com/services/affiliateservices.asmx")]
-        public string Subtext_Web_com_community_credit_www_AffiliateServices {
-            get {
-                return ((string)(this["Subtext_Web_com_community_credit_www_AffiliateServices"]));
-            }
-        }
     }
 }
