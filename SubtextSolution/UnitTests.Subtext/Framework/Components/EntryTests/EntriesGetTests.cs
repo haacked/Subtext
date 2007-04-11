dc28c3945c4d8b43ebff9c4a3476f2dc4d950756
haacked <haacked@483c6c4e-18e1-11de-b208-e5a9466b8568>
2007-04-11 07:17:36 +0000

    Missing a RollBack on a unit test. Also forgot to generate a random email address.

--- SubtextSolution/UnitTests.Subtext/Framework/Components/EntryTests/EntriesGetTests.cs
+++ SubtextSolution/UnitTests.Subtext/Framework/Components/EntryTests/EntriesGetTests.cs
@@ -12,10 +12,11 @@ namespace UnitTests.Subtext.Framework.Components.EntryTests
 	public class EntriesGetTests
 	{
 		[Test]
+		[RollBack]
 		public void CanGetRecentPosts()
 		{
 			string hostname = UnitTestHelper.GenerateRandomString();
-			Assert.IsTrue(Config.CreateBlog("", "username", "password", hostname, ""));
+			Assert.IsTrue(Config.CreateBlog("", "username", UnitTestHelper.GenerateRandomString(), "password", hostname, ""));
 			UnitTestHelper.SetHttpContextWithBlogRequest(hostname, "", "");
 
 			int blogId = Config.CurrentBlog.Id;
