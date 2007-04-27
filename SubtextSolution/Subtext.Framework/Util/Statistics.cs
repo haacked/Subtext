e0e7861e6979c37a69490f9f095f41727afbe9d0
haacked <haacked@483c6c4e-18e1-11de-b208-e5a9466b8568>
2007-04-27 00:33:11 +0000

    Adding missing files that should've been added via the merge. Grrr...

---
+++ SubtextSolution/Subtext.Framework/Util/Statistics.cs
@@ -0,0 +1,74 @@
+#region Disclaimer/Info
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Subtext WebLog
+// 
+// Subtext is an open source weblog system that is a fork of the .TEXT
+// weblog system.
+//
+// For updated news and information please visit http://subtextproject.com/
+// Subtext is hosted at SourceForge at http://sourceforge.net/projects/subtext
+// The development mailing list is at subtext-devs@lists.sourceforge.net 
+//
+// This project is licensed under the BSD license.  See the License.txt file for more information.
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// This class adapted from Rama Krishna Vavilala's Tag Cloud control article on 
+// Code Project: http://www.codeproject.com/useritems/cloud.asp
+#endregion
+
+using System;
+using System.Collections.Generic;
+
+namespace Subtext.Framework.Util
+{
+	/// <summary>
+	/// Statistical functions
+	/// </summary>
+	public static class Statistics
+	{
+		public static double Mean(IEnumerable<double> values)
+		{
+			double sum = 0;
+			int count = 0;
+
+			foreach (double d in values)
+			{
+				sum += d;
+				count++;
+			}
+
+			return sum / count;
+		}
+		
+		public static double StdDev(IEnumerable<double> values, out double mean)
+		{
+			mean = Statistics.Mean(values);
+			double sumOfDiffSquares = 0;
+			int count = 0;
+
+			foreach (double d in values)
+			{
+				double diff = (d - mean);
+				sumOfDiffSquares += diff * diff;
+				count++;
+			}
+
+			return Math.Sqrt(sumOfDiffSquares / count);
+		}
+
+        public static double StdDev<TValue>(IEnumerable<TValue> values, out double mean)
+        {
+            List<double> converted = new List<double>();
+            foreach (TValue value in values)
+            {
+                converted.Add(Convert.ToDouble(value));
+            }
+            return StdDev(converted, out mean);
+        }
+
+		public static double StdDev(IEnumerable<double> values)
+		{
+			double mean;
+			return StdDev(values, out mean);
+		}	
+	}
+}
---
+++ SubtextSolution/Subtext.Web/UI/Controls/TagCloud.cs
@@ -0,0 +1,75 @@
+#region Disclaimer/Info
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Subtext WebLog
+// 
+// Subtext is an open source weblog system that is a fork of the .TEXT
+// weblog system.
+//
+// For updated news and information please visit http://subtextproject.com/
+// Subtext is hosted at SourceForge at http://sourceforge.net/projects/subtext
+// The development mailing list is at subtext-devs@lists.sourceforge.net 
+//
+// This project is licensed under the BSD license.  See the License.txt file for more information.
+///////////////////////////////////////////////////////////////////////////////////////////////////
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Web.UI.WebControls;
+using Subtext.Framework.Data;
+using Subtext.Framework.Components;
+using Subtext.Framework.Configuration;
+using Subtext.Framework;
+
+namespace Subtext.Web.UI.Controls
+{
+    public class TagCloud : BaseControl
+    {
+        private IEnumerable<Tag> tags;
+        public IEnumerable<Tag> TagItems
+        {
+            get { return tags; }
+            set { tags = value; }
+        }
+
+        private int itemCount;
+        
+		[DefaultValue(20)]
+        public int ItemCount
+        {
+            get { return itemCount; }
+            set { itemCount = value; }
+        }
+
+        protected virtual void Tags_ItemDataBound(object sender, RepeaterItemEventArgs e)
+        {
+            if (e.Item.ItemType == ListItemType.Item || e.Item.ItemType == ListItemType.AlternatingItem)
+            {
+                Tag tag = (Tag)e.Item.DataItem;
+                HyperLink tagLink = e.Item.FindControl("TagUrl") as HyperLink;
+				if(tagLink != null)
+					tagLink.NavigateUrl = string.Format("{0}Tags/{1}/default.aspx", Config.CurrentBlog.RootUrl, tag.TagName);
+            }
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+
+            tags = Cacher.GetTopTags(ItemCount, CacheDuration.Short);
+            Repeater tagRepeater = this.FindControl("Tags") as Repeater;
+            if (tagRepeater != null)
+            {
+                tagRepeater.DataSource = TagItems;
+                tagRepeater.DataBind();
+            }
+
+            HyperLink hlDefault = this.FindControl("DefaultTagLink") as HyperLink;
+            if (hlDefault != null)
+            {
+                hlDefault.NavigateUrl = string.Format("{0}Tags/default.aspx", Config.CurrentBlog.RootUrl);
+            }
+        }
+    }
+}
