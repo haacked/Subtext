5f05c59a0f055b0ff99f4d453b510defdc98d1ae
simo <simo@483c6c4e-18e1-11de-b208-e5a9466b8568>
2007-05-11 02:53:49 +0000

    The merge messed up the SQL launch batch file (was creating the DB in the 1.9 db instead of the 2.0 one)

--- Utilities/Testing/InstallDatabaseLocally.bat
+++ Utilities/Testing/InstallDatabaseLocally.bat
@@ -3,7 +3,7 @@ REM To run this script you have to replace all templated parameter inside the sq
 
 ::SET VARIABLES
 SET DBNAME=%1%
-IF "%DBNAME%" == "" SET DBNAME=SubtextData_1.9
+IF "%DBNAME%" == "" SET DBNAME=SubtextData
 
 OSQL -E -d %DBNAME% -i ..\..\SubTextSolution\Subtext.Installation\Scripts\Installation.01.00.00.sql
 OSQL -E -d %DBNAME% -i ..\..\SubTextSolution\Subtext.Installation\Scripts\Installation.01.05.00.sql
