57858117ba39054a297ad3751fcb72e130306ab9
Haacked <haacked@gmail.com>
2007-05-13 06:17:05 +0000

    CanGetInstallationStatus test keeps failing. Making sure the subtext_Version record is in the database as part of this test.

--- SubtextSolution/UnitTests.Subtext/Installation/InstallerTests.cs
+++ SubtextSolution/UnitTests.Subtext/Installation/InstallerTests.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Configuration;
+using System.Data.SqlClient;
 using MbUnit.Framework;
 using Subtext.Installation;
 
@@ -23,6 +25,16 @@ namespace UnitTests.Subtext.Installation
 		[Test]
 		public void CanGetInstallationStatus()
 		{
+			using (SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["subtextData"].ConnectionString))
+			{
+				connection.Open();
+				using (SqlTransaction transaction = connection.BeginTransaction())
+				{
+					SqlInstaller installer = new SqlInstaller(ConfigurationManager.ConnectionStrings["subtextData"].ConnectionString);
+					Console.WriteLine("UPDATING INSTALLATION VERSION TO '{0}'!", installer.SubtextAssemblyVersion);
+					installer.UpdateInstallationVersionNumber(installer.SubtextAssemblyVersion, transaction);
+				}
+			}
 			Assert.AreEqual(InstallationState.Complete, Installer.InstallationStatus);
 		}
 	}
