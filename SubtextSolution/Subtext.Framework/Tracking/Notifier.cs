8751bc7cd5e18e28f2cb842b956c34e8e549155b
Haacked <haacked@gmail.com>
2007-05-28 06:04:42 +0000

    Removed the Notify method from Stats.cs. It wasn't being used. That method had been moved to Notifier.cs. Refactored Notifier.cs so that a problem posting to Weblogs.com would not affect posting trackbacks/pingbacks.

--- SubtextSolution/Subtext.Framework/Tracking/Notifier.cs
+++ SubtextSolution/Subtext.Framework/Tracking/Notifier.cs
@@ -1,6 +1,5 @@
 using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Web;
 using Subtext.Framework.Configuration;
 using System.Collections.Specialized;
 using Subtext.Framework.Text;
@@ -15,8 +14,6 @@ namespace Subtext.Framework.Tracking
 	public class Notifier
 	{
 		static Logging.Log Log = new Logging.Log();
-
-		public Notifier() { }
 
 		private string description;
 		public string Description
@@ -81,44 +78,53 @@ namespace Subtext.Framework.Tracking
 			Configuration.Tracking track = Config.Settings.Tracking;
 
 			//First, ping weblogs.com
-			if (track.PingWeblogs)
+			try
 			{
-				using (WeblogsNotificatinProxy weblogs = new WeblogsNotificatinProxy())
-				{
-					weblogs.Ping(BlogName, FullyQualifiedUrl);
-				}
+				PingWeblogsDotCom(track);
+			}
+			catch(HttpException e)
+			{
+				Log.Warn("Had a problem pinging weblogs.com. continuing.", e);
 			}
 
 			//Get the links from the last post
 			StringCollection links = HtmlHelper.GetLinks(Text);
 
 			//Do we have links
-			if (links != null && links.Count > 0)
+			if (links == null || links.Count == 0)
+				return;
+			
+			//Create our notification Components
+			using (PingBackNotificatinProxy pbnp = new PingBackNotificatinProxy())
 			{
-				//Create our notification Components
-				using (PingBackNotificatinProxy pbnp = new PingBackNotificatinProxy())
+				//for each link, try to pingback and/or trackback
+				foreach (string link in links)
 				{
-					//for each link, try to pingback and/or trackback
-					foreach (string link in links)
-					{
-						//get the page text
-						Uri url = HtmlHelper.ParseUri(link);
-						if (url == null)
-							continue;
+					//get the page text
+					Uri url = HtmlHelper.ParseUri(link);
+					if (url == null)
+						continue;
 
-						string pageText = HttpHelper.GetPageText(url);
-						if (pageText != null)
-						{
-							if (track.EnableTrackBacks)
-							{
-								if (!TrackBackNotificationProxy.TrackBackPing(pageText, url, Title, PostUrl, BlogName, Description) && track.EnablePingBacks)
-								{
-									Log.DebugFormat("Trackback failed to '{0}'. Let's try a PingBack.", url);
-									pbnp.Ping(pageText, PostUrl, url);
-								}
-							}
-						}
-					}
+					string pageText = HttpHelper.GetPageText(url);
+					if (pageText == null || !track.EnableTrackBacks)
+						continue;
+					
+					if (!TrackBackNotificationProxy.TrackBackPing(pageText, url, Title, PostUrl, BlogName, Description) && track.EnablePingBacks)
+					{
+						Log.DebugFormat("Trackback failed to '{0}'. Let's try a PingBack.", url);
+						pbnp.Ping(pageText, PostUrl, url);
+					}				
+				}
+			}
+		}
+
+		private void PingWeblogsDotCom(Configuration.Tracking track)
+		{
+			if (track.PingWeblogs)
+			{
+				using (WeblogsNotificatinProxy weblogs = new WeblogsNotificatinProxy())
+				{
+					weblogs.Ping(BlogName, FullyQualifiedUrl);
 				}
 			}
 		}
