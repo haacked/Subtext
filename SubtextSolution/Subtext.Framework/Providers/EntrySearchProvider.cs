569e4d6e201999ac5bf78984785de46c9f25b697
Haacked <haacked@gmail.com>
2007-06-02 06:49:58 +0000

    Added Search Unit Tests.

--- SubtextSolution/Subtext.Framework/Providers/EntrySearchProvider.cs
+++ SubtextSolution/Subtext.Framework/Providers/EntrySearchProvider.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Data;
-using System.Data.SqlClient;
 using Subtext.Data;
 using Subtext.Extensibility.Providers;
 using Subtext.Framework.Components;
