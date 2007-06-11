6010d548030830ba5b6964b4cb448953d8030eb5
haacked <haacked@483c6c4e-18e1-11de-b208-e5a9466b8568>
2007-06-11 18:25:10 +0000

    Corrected the copyright information.

--- SubtextSolution/SolutionInfo.cs
+++ SubtextSolution/SolutionInfo.cs
@@ -16,7 +16,7 @@
 using System.Reflection;
 [assembly: AssemblyCompany("Subtext")]
 [assembly: AssemblyProduct("Subtext Blog Engine")]
-[assembly: AssemblyCopyright("Copyright © VelocIT Inc. 2005-2007 - BSD License")]
+[assembly: AssemblyCopyright("Copyright © Scott Watermasysk 2003. Copyright © Phil Haack. 2005-2007 - BSD License")]
 /* [assembly: AssemblyVersion("1.0.4.0")] moved to VersionInfo.cs for automatic generation */
 /* Removed [AssemblyFileVersion] since it defaults to AssemblyVersion */
 /* Human Readable version. */
