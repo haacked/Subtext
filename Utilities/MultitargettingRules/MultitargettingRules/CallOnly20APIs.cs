83dea45eb7b3b8cabfd67b3b95886639ea8c6844
sdorman <sdorman@483c6c4e-18e1-11de-b208-e5a9466b8568>
2007-10-07 20:12:00 +0000

    Added new FxCop rule assembly for Multi-Targeting checks. Also added the solution for the rule assembly and a test project, in both VS2005 and VS2008 formats.

---
+++ Utilities/MultitargettingRules/MultitargettingRules/CallOnly20APIs.cs
@@ -0,0 +1,15 @@
+﻿using System.Resources;
+using System.IO;
+using System.Reflection;
+using Krzysztof.MultitargettingRules.Base;
+
+namespace Krzysztof.MultitargettingRules
+{
+    public class CallOnly20APIs : DontUseAPIsRuleBase
+    {
+        public CallOnly20APIs()
+            : base("CallOnly20APIs")
+        {
+        }
+    }
+}
---
+++ Utilities/MultitargettingRules/Test/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+using System.Runtime;
+using System.Security;
+using System.Runtime.ConstrainedExecution;
+using System.Reflection.Emit;
+using System.Reflection;
+using System.Collections.Generic;
+
+namespace Test {
+    class Program {
+        static void Main() {
+            Console.WriteLine("LatencyMode = {0}", GCSettings.LatencyMode);
+
+            DateTimeOffset dto = DateTimeOffset.Now;
+            Console.WriteLine("It's {0} now", dto);
+
+            AppDomainSetup a = new AppDomainSetup();
+            a.SandboxInterop = true;
+
+            Console.WriteLine(GCCollectionMode.Default);
+
+            Console.WriteLine(System.Deployment.Internal.InternalActivationContextHelper.GetApplicationManifestBytes(null));
+
+            SecureString str = new SecureString();
+            object obj = str;
+            CriticalFinalizerObject cfo4 = obj as CriticalFinalizerObject;
+
+            DynamicMethod dm = new DynamicMethod("", null, null);
+
+            SignatureHelper.GetPropertySigHelper(null, CallingConventions.Any, typeof(object), null, null, null, null, null);
+            TypeBuilder tb = null;
+            tb.DefineProperty("", PropertyAttributes.None, CallingConventions.Any, typeof(object), null, null, null, null, null);
+
+            AppDomain ad = null;
+            ad.DefineDynamicAssembly(new AssemblyName(), AssemblyBuilderAccess.Run, new List<CustomAttributeBuilder>());
+            ad.DefineDynamicAssembly(new AssemblyName(), AssemblyBuilderAccess.Run, "", null, null, null, null, true, null);
+        }
+    }
+}
