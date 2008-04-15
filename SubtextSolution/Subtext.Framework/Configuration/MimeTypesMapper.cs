8a5f9eacefda43fa92d274bf46648c6913ec5c7e
simo <simo@483c6c4e-18e1-11de-b208-e5a9466b8568>
2008-04-15 12:01:16 +0000

    Added logic and tests for automatic detection of MimeType based on file extension

---
+++ SubtextSolution/Subtext.Framework/Configuration/MimeTypesMapper.cs
@@ -0,0 +1,78 @@
+﻿#region Disclaimer/Info
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Subtext WebLog
+// 
+// Subtext is an open source weblog system that is a fork of the .TEXT
+// weblog system.
+//
+// For updated news and information please visit http://subtextproject.com/
+// Subtext is hosted at SourceForge at http://sourceforge.net/projects/subtext
+// The development mailing list is at subtext-devs@lists.sourceforge.net 
+//
+// This project is licensed under the BSD license.  See the License.txt file for more information.
+///////////////////////////////////////////////////////////////////////////////////////////////////
+#endregion
+
+
+using System;
+using System.Collections.Specialized;
+using System.Configuration;
+using System.Globalization;
+using System.IO;
+using log4net;
+using Subtext.Framework.Logging;
+
+namespace Subtext.Framework.Configuration
+{
+    public class MimeTypesMapper
+    {
+        NameValueCollection _config = null;
+        static MimeTypesMapper _mappings = new MimeTypesMapper((NameValueCollection)ConfigurationManager.GetSection("EnclosureMimetypes"));
+
+        public static MimeTypesMapper Mappings
+        {
+            get { return _mappings; }
+        }
+
+        private int _count=0;
+
+        public int Count
+        {
+            get { return _count; }
+        }
+
+        public MimeTypesMapper(NameValueCollection config)
+        {
+            if (config == null)
+                throw new ArgumentNullException("config");
+            _config = config;
+            _count = _config.Keys.Count;
+        }
+
+        public string GetMimeType(string ext)
+        {
+            if(ext==null)
+                throw new ArgumentNullException("ext","The file extension cannot be null.");
+            if (_config[ext] != null)
+                return _config[ext];
+            return null;
+        }
+
+        public string ParseUrl(string url)
+        {
+            if (url == null)
+                throw new ArgumentNullException("url", "The url cannot be null.");
+            Uri uri;
+
+            if (!Uri.TryCreate(url,UriKind.Absolute,out uri))
+                throw new ArgumentException("Url not valid.","url");
+
+            string path = uri.GetComponents(UriComponents.Path,UriFormat.SafeUnescaped);
+
+            string ext = Path.GetExtension(path);
+
+            return GetMimeType(ext);
+            
+        }
+    }
+}
---
+++ SubtextSolution/UnitTests.Subtext/Framework/Components/EnclosureTests/MimetypeDetectionTests.cs
@@ -0,0 +1,58 @@
+﻿#region Disclaimer/Info
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Subtext WebLog
+// 
+// Subtext is an open source weblog system that is a fork of the .TEXT
+// weblog system.
+//
+// For updated news and information please visit http://subtextproject.com/
+// Subtext is hosted at SourceForge at http://sourceforge.net/projects/subtext
+// The development mailing list is at subtext-devs@lists.sourceforge.net 
+//
+// This project is licensed under the BSD license.  See the License.txt file for more information.
+///////////////////////////////////////////////////////////////////////////////////////////////////
+#endregion
+
+using System;
+using MbUnit.Framework;
+using Subtext.Framework.Configuration;
+
+namespace UnitTests.Subtext.Framework.Components.EnclosureTests
+{
+    [TestFixture]
+    public class MimetypeDetectionTests
+    {
+        [Test]
+        public void CanReadMimetypeMappings()
+        {
+            Assert.AreEqual(6, MimeTypesMapper.Mappings.Count);
+        }
+
+        [RowTest]
+        [Row(".mp3","audio/mpeg")]
+        [Row(".zip","application/octetstream")]
+        [Row(".pdf","application/octetstream")]
+        [Row(".mp4","video/mp4")]
+        [Row(".avi",null)]
+        [Row(null,null, ExpectedException = typeof(ArgumentNullException))]
+        public void MimetypeAreMappedCorrectly(string ext, string expectedType)
+        {
+            Assert.AreEqual(expectedType, MimeTypesMapper.Mappings.GetMimeType(ext));
+        }
+
+        [RowTest]
+        [Row("http://mywonderfulldomain.com/podcast/episode1.mp3", "audio/mpeg")]
+        [Row("http://code.google.com/codeclimbercommons/items/download/linklift-src.1.0.zip", "application/octetstream")]
+        [Row("http://polimi.it/ingdelsoftware/Corso di primo livello/lezione1.pdf", "application/octetstream")]
+        [Row("http://wekarod.com/mvcscreencasts/screencast3.mp4", "video/mp4")]
+        [Row("http://wekarod.com/mvcscreencasts/screencast3", null)]
+        [Row("http://wekarod.com/mvcscreencasts/screencast3.qt", null)]
+        [Row(null, null, ExpectedException = typeof(ArgumentNullException))]
+        [Row("not/a valid\\url", null, ExpectedException = typeof(ArgumentException))]
+        public void CanDetectCorrectMimeType(string url, string expectedType)
+        {
+            Assert.AreEqual(expectedType,MimeTypesMapper.Mappings.ParseUrl(url));
+        }
+
+    }
+}
