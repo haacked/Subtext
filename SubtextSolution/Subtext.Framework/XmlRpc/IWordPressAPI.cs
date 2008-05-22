f83bde812e454cfd508b15c923fb5d2773554c70
twyford <twyford@483c6c4e-18e1-11de-b208-e5a9466b8568>
2008-05-22 21:09:54 +0000

    Adds in feature request #1953749 Live Writer enhancement support Thanks Tim!

---
+++ SubtextSolution/Subtext.Framework/XmlRpc/IWordPressAPI.cs
@@ -0,0 +1,21 @@
+﻿﻿using System;
+using CookComputing.XmlRpc;
+
+namespace Subtext.Framework.XmlRpc
+{
+    public struct WordpressCategory
+    {
+        public string name;
+    }
+
+    public interface IWordpressApi
+    {
+        [XmlRpcMethod("wp.newCategory",
+            Description = "Adds a new category to the blog engine.")]
+        int newCategory(
+          string blogid,
+          string username,
+          string password,
+          WordpressCategory category);
+    }
+}
