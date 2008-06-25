8f429950349fac8c2da85b62ac8fe164f9512707
timheuer <timheuer@483c6c4e-18e1-11de-b208-e5a9466b8568>
2008-06-25 21:23:36 +0000

    Fixed implemented override of BaseSyndicationHandler

--- SubtextSolution/Subtext.Framework/Syndication/RssTagHandler.cs
+++ SubtextSolution/Subtext.Framework/Syndication/RssTagHandler.cs
@@ -86,7 +86,7 @@ namespace Subtext.Framework.Syndication
         /// Gets the syndication writer.
         /// </summary>
         /// <returns></returns>
-        protected override BaseSyndicationWriter<Entry> SyndicationWriter
+        protected override BaseSyndicationWriter SyndicationWriter
         {
             get
             {
