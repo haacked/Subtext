73f67e1927c721a45a26d3b4e8c7e8738e2706aa
haacked <haacked@483c6c4e-18e1-11de-b208-e5a9466b8568>
2008-11-28 07:00:12 +0000

    Added a project specifically for managing CCNET. In particular, added a T4 template for generating the ccnet.config file. Now when we want to create a new release branch, we can just update the settings at the very top and BOOM! New ccnet.config. Isn't codegen fun! :)

--- SubtextSolution/VersionInfo.Designer.cs
+++ SubtextSolution/VersionInfo.Designer.cs
@@ -11,5 +11,5 @@ using System.Reflection;
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: AssemblyVersionAttribute("2.1.0.0")]
+[assembly: AssemblyVersionAttribute("2.1.1.0")]
 
