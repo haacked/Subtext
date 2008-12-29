55b9e15252964867e32dffadc4d42db0ccd0386f
Haacked <haacked@gmail.com>
2008-12-29 04:09:52 +0000

    Just got rid of the DbProvider and SqlDataProvider which are no longer needed. Removed the config for them too.

--- SubtextSolution/Subtext.Framework/Providers/SearchEngine.cs
+++ SubtextSolution/Subtext.Framework/Providers/SearchEngine.cs
@@ -1,3 +1,18 @@
+#region Disclaimer/Info
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Subtext WebLog
+// 
+// Subtext is an open source weblog system that is a fork of the .TEXT
+// weblog system.
+//
+// For updated news and information please visit http://subtextproject.com/
+// Subtext is hosted at SourceForge at http://sourceforge.net/projects/subtext
+// The development mailing list is at subtext-devs@lists.sourceforge.net 
+//
+// This project is licensed under the BSD license.  See the License.txt file for more information.
+///////////////////////////////////////////////////////////////////////////////////////////////////
+#endregion
+
 using System.Collections.Generic;
 using System.Data;
 using Subtext.Framework.Components;
@@ -6,13 +21,13 @@ using Subtext.Framework.Routing;
 
 namespace Subtext.Framework.Providers
 {
-	public class EntrySearchProvider
+	public class SearchEngine
 	{
         StoredProcedures _procedures = null;
         UrlHelper _urlHelper;
         Blog _blog;
 
-        public EntrySearchProvider(Blog blog, UrlHelper urlHelper, string connectionString)
+        public SearchEngine(Blog blog, UrlHelper urlHelper, string connectionString)
         {
             _blog = blog;
             _procedures = new StoredProcedures(connectionString);
