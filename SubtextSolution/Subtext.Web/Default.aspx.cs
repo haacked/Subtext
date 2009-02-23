f86e447a66bb1d60651b8503a0a745b480f7e931
haacked <haacked@483c6c4e-18e1-11de-b208-e5a9466b8568>
2009-02-23 05:24:15 +0000

    Adding a Default.aspx file to compensate for a Cassini issue when making requests for the root. Note: This file is not needed for IIS integrated.

---
+++ SubtextSolution/Subtext.Web/Default.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System.Web;
+using System.Web.Routing;
+using System.Web.UI;
+using Subtext.Framework;
+using Subtext.Framework.Routing;
+
+namespace Subtext.Web
+{
+    public partial class _Default : Page
+    {
+        public void Page_Load(object sender, System.EventArgs e)
+        {
+            //Workaround for Cassini issue with request to /
+            //In IIS7, Default.aspx can be deleted.
+            var route = new RootRoute(HostInfo.Instance.BlogAggregationEnabled);
+            var httpContext = new HttpContextWrapper(HttpContext.Current);
+            RouteData routeData = route.GetRouteData(httpContext);
+            RequestContext requestContext = new RequestContext(httpContext, routeData);
+            string originalPath = Request.Path;
+            HttpContext.Current.RewritePath(Request.ApplicationPath, false);
+            IRouteHandler routeHandler = new PageRouteHandler(HostInfo.Instance.BlogAggregationEnabled ? "~/AggDefault.aspx" : "~/Dtp.aspx");
+            IHttpHandler httpHandler = routeHandler.GetHttpHandler(requestContext);
+            httpHandler.ProcessRequest(HttpContext.Current);
+            HttpContext.Current.RewritePath(originalPath);
+        }
+    }
+}
