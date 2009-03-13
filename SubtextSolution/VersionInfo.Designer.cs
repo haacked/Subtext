a04221dc85d6e8baa2586c43f69be86418c2d437
Haacked <haacked@gmail.com>
2009-03-13 05:25:20 +0000

    Incrementing trunk version.

--- SubtextSolution/VersionInfo.Designer.cs
+++ SubtextSolution/VersionInfo.Designer.cs
@@ -11,5 +11,5 @@ using System.Reflection;
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: AssemblyVersionAttribute("2.1.1.0")]
+[assembly: AssemblyVersionAttribute("2.2.0.0")]
 
