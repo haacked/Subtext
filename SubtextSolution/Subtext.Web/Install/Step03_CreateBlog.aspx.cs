91eaf8c9eb4c12d2f26a94cb16bfa15338f1d311
haacked <haacked@483c6c4e-18e1-11de-b208-e5a9466b8568>
2009-03-13 06:25:21 +0000

    Fix:2 - Email address now saved when creating a new blog.

--- SubtextSolution/Subtext.Web/Install/Step03_CreateBlog.aspx.cs
+++ SubtextSolution/Subtext.Web/Install/Step03_CreateBlog.aspx.cs
@@ -84,6 +84,11 @@ namespace Subtext.Web.Install
 
 			if(Config.CreateBlog("TEMPORARY BLOG NAME", HostInfo.Instance.HostUserName, HostInfo.Instance.Password, Request.Url.Host, string.Empty, passwordAlreadyHashed) > -1)
 			{
+                if (!String.IsNullOrEmpty(Request.QueryString["email"])) {
+                    Blog blog = Config.GetBlog(Request.Url.Host, string.Empty);
+                    blog.Email = Request.QueryString["email"];
+                    Config.UpdateConfigData(blog);
+                }
 				//We probably should have creating the blog authenticate the user 
 				//automatically so this redirect doesn't require a login.
                 InstallationManager installManager = new InstallationManager(Subtext.Extensibility.Providers.Installation.Provider);
