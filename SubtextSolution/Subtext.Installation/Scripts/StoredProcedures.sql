a17b03d96bd80172dc9b498175c697e1473a630c
haacked <haacked@483c6c4e-18e1-11de-b208-e5a9466b8568>
2009-05-27 06:40:33 +0000

    Checkin to get the build to work.

--- SubtextSolution/Subtext.Installation/Scripts/StoredProcedures.sql
+++ SubtextSolution/Subtext.Installation/Scripts/StoredProcedures.sql
@@ -5716,7 +5716,7 @@ FROM    [<dbUser,varchar,dbo>].subtext_EntryViewCount,
 WHERE   Comments.Id = EntryId
         AND [<dbUser,varchar,dbo>].Subtext_Content.Id = EntryId
 		AND [<dbUser,varchar,dbo>].Subtext_Content.BlogId = @BlogId
-		AND (@MinDate IS NULL OR [<dbUser,varchar,dbo>].Subtext_Content.DateAdded >= @MinDate)
+		/*AND (@MinDate IS NULL OR [<dbUser,varchar,dbo>].subtext_Content.DateAdded >= @MinDate)*/
 ORDER BY ( WebCount * 15 ) + ( AggCount * 10 ) + ( CommentCount * 35 ) DESC
 
 GRANT EXECUTE ON [<dbUser,varchar,dbo>].[subtext_GetPopularPosts] TO [public]
