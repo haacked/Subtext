8cf774dfe4c2c6c9a470a2aaea8bf2de2c631bcb
haacked <haacked@483c6c4e-18e1-11de-b208-e5a9466b8568>
2009-08-21 21:44:40 +0000

    Issue #20: Forgot to add this file as part of fixing issue 20.

---
+++ SubtextSolution/Subtext.Framework/Routing/SubtextRouteCollection.cs
@@ -0,0 +1,51 @@
+﻿#region Disclaimer/Info
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Subtext WebLog
+// 
+// Subtext is an open source weblog system that is a fork of the .TEXT
+// weblog system.
+//
+// For updated news and information please visit http://subtextproject.com/
+// Subtext is hosted at Google Code at http://code.google.com/p/subtext/
+// The development mailing list is at subtext-devs@lists.sourceforge.net 
+//
+// This project is licensed under the BSD license.  See the License.txt file for more information.
+///////////////////////////////////////////////////////////////////////////////////////////////////
+#endregion
+
+using System.Web.Routing;
+using Ninject;
+
+namespace Subtext.Framework.Routing
+{
+    public class SubtextRouteMapper
+    {
+        public SubtextRouteMapper(RouteCollection routes, IKernel kernel)
+        {
+            Routes = routes;
+            Kernel = kernel;
+        }
+
+        public IKernel Kernel
+        {
+            get;
+            private set;
+        }
+
+        protected RouteCollection Routes
+        {
+            get;
+            private set;
+        }
+
+        public void Add(string routeName, RouteBase route) 
+        {
+            Routes.Add(routeName, route);
+        }
+
+        public void Add(RouteBase route)
+        {
+            Routes.Add(route);
+        }
+    }
+}
