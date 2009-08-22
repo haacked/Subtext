fc9eb382bc7d5ffac6553162d9a4252a8c088f8c
Haacked <haacked@gmail.com>
2009-08-22 21:01:32 +0000

    Fixed the broken "Powered by Subtext" image on the aggregate homepage.

--- SubtextSolution/Subtext.Web/pages/AggDefault.aspx.cs
+++ SubtextSolution/Subtext.Web/pages/AggDefault.aspx.cs
@@ -18,6 +18,7 @@ using System.Configuration;
 using System.Web;
 using System.Web.UI.WebControls;
 using Subtext.Web.UI.Controls.Aggregate;
+using Subtext.Framework.Routing;
 
 namespace Subtext.Web
 {
@@ -25,69 +26,74 @@ namespace Subtext.Web
     /// When AggregateBlog is enabled in Web.config, this page renders contents from 
     /// all blogs which have set their blog posts to be included in the main blog.
     /// </summary>
-	public partial class AggDefault : AggregatePage
-	{
-		protected HyperLink Hyperlink6;
-		protected HyperLink Hyperlink7;
+    public partial class AggDefault : AggregatePage
+    {
+        protected HyperLink Hyperlink6;
+        protected HyperLink Hyperlink7;
 
-		protected void Page_Load(object sender, EventArgs e)
-		{
+        protected void Page_Load(object sender, EventArgs e)
+        {
             //No postbacks on this page. It is output cached.
-			SetStyle();
+            SetStyle();
             DataBind();
-		}
+        }
 
-		private string aggregateUrl = null;
-		/// <summary>
-		/// Url to the aggregate page.
-		/// </summary>
-		protected string AggregateUrl
-		{
-			get
-			{
-				if (this.aggregateUrl == null)
-				{
-					this.aggregateUrl = ConfigurationManager.AppSettings["AggregateUrl"];
-					if (Request.Url.Port != 80)
-					{
-						UriBuilder url = new UriBuilder(aggregateUrl);
-						url.Port = Request.Url.Port;
-						this.aggregateUrl = url.ToString();
-					}
-				}
-				return aggregateUrl;
-			}
-		}
+        private string aggregateUrl = null;
+        /// <summary>
+        /// Url to the aggregate page.
+        /// </summary>
+        protected string AggregateUrl
+        {
+            get
+            {
+                if (this.aggregateUrl == null)
+                {
+                    this.aggregateUrl = ConfigurationManager.AppSettings["AggregateUrl"];
+                    if (Request.Url.Port != 80)
+                    {
+                        UriBuilder url = new UriBuilder(aggregateUrl);
+                        url.Port = Request.Url.Port;
+                        this.aggregateUrl = url.ToString();
+                    }
+                }
+                return aggregateUrl;
+            }
+        }
 
-		private void SetStyle()
-		{
-			const string style = "<link href=\"{0}{1}\" type=\"text/css\" rel=\"stylesheet\">";
-			string apppath = HttpContext.Current.Request.ApplicationPath.EndsWith("/") ? HttpContext.Current.Request.ApplicationPath : HttpContext.Current.Request.ApplicationPath + "/";
+        protected string ImageUrl(string imageName)
+        {
+            return VirtualPathUtility.ToAbsolute("~/images/" + imageName);
+        }
+
+        private void SetStyle()
+        {
+            const string style = "<link href=\"{0}{1}\" type=\"text/css\" rel=\"stylesheet\">";
+            string apppath = HttpContext.Current.Request.ApplicationPath.EndsWith("/") ? HttpContext.Current.Request.ApplicationPath : HttpContext.Current.Request.ApplicationPath + "/";
             //TODO: This is hard-coded to look in the simple skin for aggregate blogs. We should change this later.
             Style.Text = string.Format(style, apppath, "Skins/Aggregate/Simple/Style.css") + "\n" + string.Format(style, apppath, "Skins/Aggregate/Simple/blue.css") + "\n" + string.Format(style, apppath, "Scripts/jquery.lightbox-0.5.css");
-		}
+        }
 
-		#region Web Form Designer generated code
-		override protected void OnInit(EventArgs e)
-		{
-			//
-			// CODEGEN: This call is required by the ASP.NET Web Form Designer.
-			//
-			InitializeComponent();
-			base.OnInit(e);
-		}
-		
-		/// <summary>
-		/// Required method for Designer support - do not modify
-		/// the contents of this method with the code editor.
-		/// </summary>
-		private void InitializeComponent()
-		{    
-			this.Load += new EventHandler(this.Page_Load);
+        #region Web Form Designer generated code
+        override protected void OnInit(EventArgs e)
+        {
+            //
+            // CODEGEN: This call is required by the ASP.NET Web Form Designer.
+            //
+            InitializeComponent();
+            base.OnInit(e);
+        }
 
-		}
-		#endregion
-	}
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Load += new EventHandler(this.Page_Load);
+
+        }
+        #endregion
+    }
 }
 
 
