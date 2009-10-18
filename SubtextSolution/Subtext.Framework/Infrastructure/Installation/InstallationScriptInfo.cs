25dcd2a60a353302d277ef3e179d5fe980b8e2b1
haacked <haacked@483c6c4e-18e1-11de-b208-e5a9466b8568>
2009-10-18 06:52:11 +0000

    Forgot to add this file.

---
+++ SubtextSolution/Subtext.Framework/Infrastructure/Installation/InstallationScriptInfo.cs
@@ -0,0 +1,53 @@
+﻿#region Disclaimer/Info
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Subtext WebLog
+// 
+// Subtext is an open source weblog system that is a fork of the .TEXT
+// weblog system.
+//
+// For updated news and information please visit http://subtextproject.com/
+// Subtext is hosted at Google Code at http://code.google.com/p/subtext/
+// The development mailing list is at subtext-devs@lists.sourceforge.net 
+//
+// This project is licensed under the BSD license.  See the License.txt file for more information.
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+#endregion
+
+using System;
+using System.Text.RegularExpressions;
+
+namespace Subtext.Framework.Infrastructure.Installation
+{
+    internal class InstallationScriptInfo
+    {
+        //Have the compiled regex as static to get the full benefit of compilation
+        private static readonly Regex ScriptParseRegex =
+            new Regex(@"(?<ScriptName>Installation\.(?<version>\d+\.\d+\.\d+)\.sql)$",
+                      RegexOptions.Compiled | RegexOptions.IgnoreCase);
+
+        private InstallationScriptInfo(string scriptName, Version version)
+        {
+            Version = version;
+            ScriptName = scriptName;
+        }
+
+        public string ScriptName { get; set; }
+
+        public Version Version { get; set; }
+
+        internal static InstallationScriptInfo Parse(string resourceName)
+        {
+            Match match = ScriptParseRegex.Match(resourceName);
+            if(!match.Success)
+            {
+                return null;
+            }
+            var version = new Version(match.Groups["version"].Value);
+            string scriptName = match.Groups["ScriptName"].Value;
+            return new InstallationScriptInfo(scriptName, version);
+        }
+    }
+
+}
