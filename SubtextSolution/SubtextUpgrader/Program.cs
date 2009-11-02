6911c88e96292d815c1f211212b2fa9fe7e4af0d
Haacked <haacked@gmail.com>
2009-11-02 05:16:56 +0000

    Adding a draft of the upgrade console app. This is a very rough prototype with no unit tests yet.

---
+++ SubtextSolution/SubtextUpgrader/Program.cs
@@ -0,0 +1,54 @@
+﻿#region Disclaimer/Info
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Subtext WebLog
+// 
+// Subtext is an open source weblog system that is a fork of the .TEXT
+// weblog system.
+//
+// For updated news and information please visit http://subtextproject.com/
+// Subtext is hosted at Google Code at http://code.google.com/p/subtext/
+// The development mailing list is at subtext@googlegroups.com 
+//
+// This project is licensed under the BSD license.  See the License.txt file for more information.
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+#endregion
+
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace SubtextUpgrader
+{
+    /// <summary>
+    /// This tool is used to help upgrade existing installations of 
+    /// Subtext to the latest version.
+    /// </summary>
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string destinationPath;
+            if(args.Length == 0)
+            {
+                Console.WriteLine("Please enter the destination path and hit ENTER:");
+                destinationPath = Console.ReadLine();
+            }
+            else
+            {
+                destinationPath = args[0];
+            }
+
+            var sourceDirectory = new DirectoryInfo(Assembly.GetExecutingAssembly().Location);
+            var destinationDirectory = new DirectoryInfo(destinationPath);
+            
+            var skinUpgrader = new SkinUpgrader();
+            skinUpgrader.UpgradeCustomSkins(destinationDirectory);
+
+            var deployer = new FileDeployer();
+            deployer.CopyDirectory(sourceDirectory, destinationDirectory);
+            deployer.RemoveOldDirectories(destinationDirectory);
+        }
+    }
+}
---
+++ SubtextSolution/SubtextUpgrader/XmlExtensions.cs
@@ -0,0 +1,44 @@
+﻿#region Disclaimer/Info
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Subtext WebLog
+// 
+// Subtext is an open source weblog system that is a fork of the .TEXT
+// weblog system.
+//
+// For updated news and information please visit http://subtextproject.com/
+// Subtext is hosted at Google Code at http://code.google.com/p/subtext/
+// The development mailing list is at subtext@googlegroups.com 
+//
+// This project is licensed under the BSD license.  See the License.txt file for more information.
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+#endregion
+
+using System.Xml;
+
+namespace SubtextUpgrader
+{
+    public static class XmlExtensions
+    {
+        public static XmlNode SelectConnectionStringsNode(this XmlNode node)
+        {
+            return node.SelectSingleNode("/configuration/connectionStrings");
+        }
+
+        public static XmlNode SelectFriendlyUrlSettingsNode(this XmlNode node)
+        {
+            return node.SelectSingleNode("/configuration/FriendlyUrlSettings");
+        }
+
+        public static XmlNode SelectEnclosureMimetypesNode(this XmlNode node)
+        {
+            return node.SelectSingleNode("/configuration/EnclosureMimetypes");
+        }
+        
+        public static XmlNode SelectEmailNode(this XmlNode node)
+        {
+            return node.SelectSingleNode("/configuration/Email");
+        }
+    }
+}
