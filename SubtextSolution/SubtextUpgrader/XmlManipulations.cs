43559b6bacd9ea5e394ec7c383a69a7ce4427dd1
haacked <haacked@483c6c4e-18e1-11de-b208-e5a9466b8568>
2009-11-08 04:56:42 +0000

    Added copyright notices

--- SubtextSolution/SubtextUpgrader/XmlManipulations.cs
+++ SubtextSolution/SubtextUpgrader/XmlManipulations.cs
@@ -1,4 +1,21 @@
-﻿using System.Collections.Generic;
+﻿#region Disclaimer/Info
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Subtext WebLog
+// 
+// Subtext is an open source weblog system that is a fork of the .TEXT
+// weblog system.
+//
+// For updated news and information please visit http://subtextproject.com/
+// Subtext is hosted at Google Code at http://code.google.com/p/subtext/
+// The development mailing list is at subtext@googlegroups.com 
+//
+// This project is licensed under the BSD license.  See the License.txt file for more information.
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+#endregion
+
+using System.Collections.Generic;
 using System.IO;
 using System.Xml;
 
