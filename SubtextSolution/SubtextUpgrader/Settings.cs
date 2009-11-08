8888ea5875cee6d407fa5a275610dc33ade550ee
haacked <haacked@483c6c4e-18e1-11de-b208-e5a9466b8568>
2009-11-08 19:41:32 +0000

    Added a command line parser from the http://cmdlineparser.codeplex.com/ project.

---
+++ SubtextSolution/SubtextUpgrader/Settings.cs
@@ -0,0 +1,34 @@
+﻿#region Disclaimer/Info
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Subtext WebLog
+// 
+// Subtext is an open source weblog system that is a fork of the .TEXT
+// weblog system.
+//
+// For updated news and information please visit http://subtextproject.com/
+// Subtext is hosted at Google Code at http://code.google.com/p/subtext/
+// The development mailing list is at subtext@googlegroups.com 
+//
+// This project is licensed under the BSD license.  See the License.txt file for more information.
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+#endregion
+
+using SubtextUpgrader.CommandLineParser.Attributes;
+using System.ComponentModel;
+
+namespace SubtextUpgrader
+{
+    public class Settings
+    {
+        [OptionName("s"), DefaultValue("'Subtext.Web' folder in the current directory"), Description("Source directory with new Subtext files")]
+        public string SourceDirectory { get; private set; }
+
+        [OptionName("t"), Description("The target directory to upgrade.")]
+        public string UpgradeTargetDirectory { get; private set; }
+
+        [OptionName("q"), Description("Quiet mode, do not prompt to continue.")]
+        public bool QuietMode { get; private set; }
+    }
+}
