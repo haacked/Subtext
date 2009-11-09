be177e0f50cf1b1ba70cfed64997aae106313c17
haacked <haacked@483c6c4e-18e1-11de-b208-e5a9466b8568>
2009-11-09 04:35:31 +0000

    Fixed some broken unit tests.

---
+++ SubtextSolution/SubtextUpgraderTests/NonDisposableMemoryStream.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+
+namespace SubtextUpgraderTests
+{
+    public class NonDisposableMemoryStream : MemoryStream
+    {
+        public override void Close()
+        {
+            // Do nothing
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            // Do Nothing
+        }
+    }
+
+}
