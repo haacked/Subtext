0c4b8e0554a21f256094afeba15c7a6246f4d993
haacked <haacked@483c6c4e-18e1-11de-b208-e5a9466b8568>
2009-11-29 22:27:01 +0000

    Fixed up paths and added new build.cmd to run NAnt build script.

--- src/ccrunner.bat
+++
@@ -1,2 +0,0 @@
-..\tools\Nant\NAnt -buildfile:SubText-CCNET.build -D:CCNetProject=Subtext -D:CCNetBuildDate=2007/08/11 -D:CCNetBuildTime=13:00:00 -D:CCNetArtifactDirectory="artifacts\output" -D:CCNetWorkingDirectory="." -D:CCNetLabel=2.0.0.0 -D:fxcop.exe="..\BuildTools\FxCop\FxCopCmd.exe" testAndCover reporting release dist.source
-pause
