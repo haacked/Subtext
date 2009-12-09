d51ade3fbeec930ef3fac42a8b1a11367d97640c
haacked <haacked@483c6c4e-18e1-11de-b208-e5a9466b8568>
2009-12-09 23:55:21 +0000

    Removed all occurrences of the addLoadEvent method. Used the jQuery approach instead.

--- src/Subtext.Web/Scripts/ExternalLinks.js
+++ src/Subtext.Web/Scripts/ExternalLinks.js
@@ -1,33 +1,12 @@
 /*
 Method for applying a specific target and style to 
 external links.  Adapted from 
-
-http://www.justinfrench.com/index.php?id=16
-and
-http://www.boagworld.com/archives/2006/01/external_links_and_new_windows.html
 */
-function externalLinks() 
-{
-    if (!document.getElementsByTagName) return;
-    var anchors = document.getElementsByTagName("a");
-    for (var i = 0; i < anchors.length; i++) 
-    {
-        var anchor = anchors[i];
-        if (anchor.getAttribute("href") && anchor.getAttribute("rel")) 
-        {
-			if(anchor.getAttribute("rel").indexOf("external") >= 0)
-			{
-				anchor.target = "_blank";
-				addClass(anchor, 'newWindowStyle');
-				
-				if(anchor.title.length > 0)
-					anchor.title = anchor.title + ' (new window)';
-				else
-					anchor.title = '(new window)';
-			}
-        }
-    }
-}
 
-// addLoadEvent is defined in Subtext.Web/Scripts/common.js
-addLoadEvent(externalLinks);
+$(function() {
+    $("a[href][rel*='external']").each(function() {
+        $(this).addClass('external');
+        $(this).attr('title', jQuery.trim($(this).attr('title') + ' (new window)'));
+        $(this).attr('target', '_blank');
+    });
+});
--- src/Subtext.Web/Scripts/common.js
+++ src/Subtext.Web/Scripts/common.js
@@ -1,25 +1,3 @@
-//
-// addLoadEvent()
-// Adds event to window.onload without overwriting currently assigned onload functions.
-// Function found at Simon Willison's weblog - http://simon.incutio.com/
-//
-function addLoadEvent(func)
-{	
-	var oldonload = window.onload;
-	if (typeof window.onload != 'function')
-	{
-    	window.onload = func;
-	} 
-	else 
-	{
-		window.onload = function()
-		{
-			oldonload();
-			func();
-		};
-	}
-}
-
 /* ===============================================
  * ================ COMMON =======================
  * Some common functions for the basic JavaScript objects
