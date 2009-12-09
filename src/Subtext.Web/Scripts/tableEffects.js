e153307aefaf594f08b45753eed82015898a8a34
haacked <haacked@483c6c4e-18e1-11de-b208-e5a9466b8568>
2009-12-09 23:41:47 +0000

    Cleaned up the table highlighting script and a bug in piyo.js

--- src/Subtext.Web/Scripts/tableEffects.js
+++ src/Subtext.Web/Scripts/tableEffects.js
@@ -1,34 +1,10 @@
-function initRowHighlighting()
-{
-	if (!document.getElementsByTagName)
-		{ return; }
-
-	var tables = getElementsByClass('highlightTable', document, 'table');
-	
-	for(var i = 0; i < tables.length; i++)
-	{
-		var table = tables[i];
-		//Make sure to use th tags for header row.
-		attachRowMouseEvents(table.getElementsByTagName('tr'));
-	}
-}
-
-function attachRowMouseEvents(rows)
-{
-	for(var i = 0; i < rows.length; i++)
-	{
-		var row = rows[i];
-		if(i%2 == 1)
-		{
-			row.onmouseover = function() { addClass(this, 'highlight'); }
-			row.onmouseout = function() { removeClass(this, 'highlight'); }
-		}
-		else
-		{
-			row.onmouseover = function() { addClass(this, 'highlightAlt'); }
-			row.onmouseout = function() { removeClass(this, 'highlightAlt'); }
-		}
-	}
-}
-
-addLoadEvent(initRowHighlighting);
+$(function() {
+    $('table.highlightTable tr').mouseover(function() {
+        $(this).addClass('highlight');
+    });
+    $('table.highlightTable tr').mouseout(function() {
+        $(this).removeClass('highlight');
+    });
+    $('table.highlightTable tr:even').addClass('even');
+    $('table.highlightTable tr:odd').addClass('odd');
+});
