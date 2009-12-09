42e7fa30e6d8ec68f61b674d50b62c5fcd6bbddd
haacked <haacked@483c6c4e-18e1-11de-b208-e5a9466b8568>
2009-12-09 19:00:45 +0000

    Fixed a bug in which the form action was incorrect in a multi-blog scenario with IIS7 Integrated mode.

---
+++ src/Subtext.Web/Controls/Form.cs
@@ -0,0 +1,55 @@
+﻿#region Disclaimer/Info
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Subtext WebLog
+// 
+// Subtext is an open source weblog system that is a fork of the .TEXT
+// weblog system.
+//
+// For updated news and information please visit http://subtextproject.com/
+// Subtext is hosted at Google Code at http://code.google.com/p/subtext/
+// The development mailing list is at subtext@googlegroups.com 
+//
+// This project is licensed under the BSD license.  See the License.txt file for more information.
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+#endregion
+
+// Adapted from Nikhil's blog post: http://www.nikhilk.net/Entry.aspx?id=182
+
+using System;
+using System.Text;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+
+namespace Subtext.Web.Controls
+{
+    public class Form : HtmlForm
+    {
+        private const string FormActionScript =
+    @"document.getElementById('{0}').action = window.location.href;
+";
+
+        private const string AjaxFormActionScript =
+    @"Sys.WebForms.PageRequestManager.getInstance().add_endRequest(function() {{
+  document.getElementById('{0}').action = window.location.href;
+}});
+";
+
+        protected override void OnPreRender(EventArgs e)
+        {
+            base.OnPreRender(e);
+
+            var sb = new StringBuilder();
+            sb.AppendFormat(FormActionScript, ClientID);
+
+            ScriptManager scriptManager = ScriptManager.GetCurrent(Page);
+            if((scriptManager != null) && scriptManager.SupportsPartialRendering)
+            {
+                sb.AppendFormat(AjaxFormActionScript, ClientID);
+            }
+
+            Page.ClientScript.RegisterStartupScript(typeof(Form), String.Empty, sb.ToString(), true/*addScriptTags*/);
+        }
+    }
+}
