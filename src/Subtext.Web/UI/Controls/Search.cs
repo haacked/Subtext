531ca00ea2d5bcd603357ac8f07b1a96be763b5c
Haacked <haacked@gmail.com>
2010-01-04 05:28:01 +0000

    Added a DIV for styling search results and removed the rel="me" (which is redundant for search results). Added a SearchResult property to the control so people writing custom skins can bind to the SearchResult property manually and add the rel="me" if they want.

--- src/Subtext.Web/UI/Controls/Search.cs
+++ src/Subtext.Web/UI/Controls/Search.cs
@@ -16,10 +16,8 @@
 #endregion
 
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Web.UI.WebControls;
-using Subtext.Framework.Data;
 using Subtext.Framework.Services.SearchEngine;
 using Subtext.Framework.Util;
 
@@ -78,9 +76,15 @@ namespace Subtext.Web.UI.Controls
         {
             if (e.Item.ItemType == ListItemType.Item || e.Item.ItemType == ListItemType.AlternatingItem)
             {
-                var pi = (SearchEngineResult)e.Item.DataItem;
-                BindLink(e, pi);
+                SearchResult = (SearchEngineResult)e.Item.DataItem;
+                BindLink(e, SearchResult);
             }
+        }
+
+        public SearchEngineResult SearchResult
+        {
+            get; 
+            private set;
         }
 
         private void BindLink(RepeaterItemEventArgs e, SearchEngineResult searchResult)
@@ -92,9 +96,14 @@ namespace Subtext.Web.UI.Controls
             {
                 relatedLink.Text = searchResult.Title;
                 relatedLink.NavigateUrl = Url.EntryUrl(searchResult);
-                relatedLink.Attributes.Add("rel", "me");
-                if (datePublished != null) datePublished.Text = searchResult.DateSyndicated.ToShortDateString();
-                if (score != null) score.Text = searchResult.Score.ToString();
+                if (datePublished != null)
+                {
+                    datePublished.Text = searchResult.DateSyndicated.ToShortDateString();
+                }
+                if (score != null)
+                {
+                    score.Text = searchResult.Score.ToString();
+                }
             }
         }
     }
