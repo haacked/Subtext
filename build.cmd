a1b0fb13d5d7e2cfcd058f95faf76f12f66b7be4
simone.chiaretta <simone.chiaretta@483c6c4e-18e1-11de-b208-e5a9466b8568>
2010-10-24 17:08:39 +0000

    Changed reference to root folder

--- build.cmd
+++ build.cmd
@@ -1,1 +1,1 @@
-tools\Nant\NAnt -verbose -buildfile:src\SubText-CCNET.build -D:build.version=2.5.1.1 -D:root.dir=".." release > build.log
+tools\Nant\NAnt -verbose -buildfile:src\SubText-CCNET.build -D:build.version=2.5.1.1 release > build.log
