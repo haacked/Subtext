854fcbf163a98d0efe02e41e768b1d0e888a87b2
Haacked <haacked@gmail.com>
2010-10-25 22:38:13 +0000

    Fixed a typo. This was calling the wrong build file.

--- build.cmd
+++ build.cmd
@@ -1,1 +1,1 @@
-tools\Nant\NAnt -verbose -buildfile:src\SubText-CCNET.build -D:build.version=2.5.1.1 release > build.log
+tools\Nant\NAnt -verbose -buildfile:src\SubText.build -D:build.version=2.5.1.1 release > build.log
