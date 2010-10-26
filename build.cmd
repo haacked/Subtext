855a5f3dcba170a8020f9a67d73721aa4b437964
simone.chiaretta <simone.chiaretta@483c6c4e-18e1-11de-b208-e5a9466b8568>
2010-10-26 22:21:24 +0000

    Added Gallio to source tree (I know, a bit big, will clean it up from references not needed later) and created a nant to see if at least testing works, will try calculating the coverage later

--- build.cmd
+++ build.cmd
@@ -1,1 +1,2 @@
-tools\Nant\NAnt -verbose -buildfile:src\SubText.build -D:build.version=2.5.1.1 release > build.log
+tools\Nant\NAnt -verbose -buildfile:src\SubText.build -D:build.version=2.6.0.1 test> build.log
+pause
