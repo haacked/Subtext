9c6682d91d8d0473217def76e6b59c5056d86552
simone.chiaretta <simone.chiaretta@483c6c4e-18e1-11de-b208-e5a9466b8568>
2010-12-03 15:10:56 +0000

    Adding scaffolding for the RTE provider

---
+++ src/Subtext.Providers.BlogEntryEditor.CKEditor/CKEBlogEntryEditorProvider.cs
@@ -0,0 +1,53 @@
+﻿#region Disclaimer/Info
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Subtext WebLog
+// 
+// Subtext is an open source weblog system that is a fork of the .TEXT
+// weblog system.
+//
+// For updated news and information please visit http://subtextproject.com/
+// Subtext is hosted at Google Code at http://code.google.com/p/subtext/
+// The development mailing list is at subtext@googlegroups.com 
+//
+// This project is licensed under the BSD license.  See the License.txt file for more information.
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+#endregion
+
+
+using System;
+using Subtext.Extensibility.Providers;
+
+namespace Subtext.Providers.BlogEntryEditor.CKEditor
+{
+    public class CKEBlogEntryEditorProvider : BlogEntryEditorProvider
+    {
+        public override string Text
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override string Xhtml
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public override System.Web.UI.Control RichTextEditorControl
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public override void InitializeControl(object subtextContext)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Subtext.Providers.BlogEntryEditor.CKEditor/Properties/AssemblyInfo.cs
@@ -0,0 +1,28 @@
+﻿#region Disclaimer/Info
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Subtext WebLog
+// 
+// Subtext is an open source weblog system that is a fork of the .TEXT
+// weblog system.
+//
+// For updated news and information please visit http://subtextproject.com/
+// Subtext is hosted at Google Code at http://code.google.com/p/subtext/
+// The development mailing list is at subtext@googlegroups.com 
+//
+// This project is licensed under the BSD license.  See the License.txt file for more information.
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+#endregion
+
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Security.Permissions;
+
+
+[assembly: AssemblyTitle("Subtext.Providers.RichTextEditor.CKEditor")]
+[assembly: AssemblyDescription("Implements the RichTextEditor Provider using the CKEditor v3.x project")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: CLSCompliant(false)]
