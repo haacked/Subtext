4fe0ef7a1165a4f60ec1dce4ca4494f5a5f93a10
Haacked <haacked@gmail.com>
2010-12-26 18:59:05 +0000

    Fixed a unit test related to RedirectPermanent again.

--- src/UnitTests.Subtext/Framework/Data/CacherTests.cs
+++ src/UnitTests.Subtext/Framework/Data/CacherTests.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Web;
 using System.Web.Caching;
@@ -14,7 +15,6 @@ using Subtext.Framework.Providers;
 using Subtext.Framework.Routing;
 using Subtext.Infrastructure;
 using UnitTests.Subtext.Framework.Util;
-using System.Globalization;
 
 namespace UnitTests.Subtext.Framework.Data
 {
@@ -227,11 +227,11 @@ namespace UnitTests.Subtext.Framework.Data
         public void GetFeedback_WithFeedbackInCache_RetrievesFromCache()
         {
             // arrange
-            var feedback = new FeedbackItem(FeedbackType.Comment) {Title = "Testing Cacher"};
-            var parentEntry = new Entry(PostType.BlogPost) {Id = 322};
+            var feedback = new FeedbackItem(FeedbackType.Comment) { Title = "Testing Cacher" };
+            var parentEntry = new Entry(PostType.BlogPost) { Id = 322 };
             var context = new Mock<ISubtextContext>();
-            context.Setup(c => c.Blog).Returns(new Blog {Id = 1001});
-            context.Setup(c => c.Cache["ParentEntry:Comments:EntryId322:BlogId1001"]).Returns(new List<FeedbackItem> {feedback});
+            context.Setup(c => c.Blog).Returns(new Blog { Id = 1001 });
+            context.Setup(c => c.Cache["ParentEntry:Comments:EntryId322:BlogId1001"]).Returns(new List<FeedbackItem> { feedback });
             context.Setup(c => c.Repository.GetFeedbackForEntry(parentEntry)).Throws(new Exception("Repository should not have been accessed"));
 
             // act
@@ -245,12 +245,12 @@ namespace UnitTests.Subtext.Framework.Data
         public void GetFeedback_WithFeedbackNotInCache_RetrievesFromRepositoryAndInsertsInCache()
         {
             // arrange
-            var feedback = new FeedbackItem(FeedbackType.Comment) {Title = "Testing Cacher"};
-            var parentEntry = new Entry(PostType.BlogPost) {Id = 322};
+            var feedback = new FeedbackItem(FeedbackType.Comment) { Title = "Testing Cacher" };
+            var parentEntry = new Entry(PostType.BlogPost) { Id = 322 };
             var context = new Mock<ISubtextContext>();
-            context.Setup(c => c.Blog).Returns(new Blog {Id = 1001});
+            context.Setup(c => c.Blog).Returns(new Blog { Id = 1001 });
             context.Setup(c => c.Cache["ParentEntry:Comments:EntryId322:BlogId1001"]).Returns(null);
-            context.Setup(c => c.Repository.GetFeedbackForEntry(parentEntry)).Returns(new List<FeedbackItem> {feedback});
+            context.Setup(c => c.Repository.GetFeedbackForEntry(parentEntry)).Returns(new List<FeedbackItem> { feedback });
 
             // act
             var cachedFeedback = Cacher.GetFeedback(parentEntry, context.Object);
@@ -299,7 +299,7 @@ namespace UnitTests.Subtext.Framework.Data
         public void GetEntry_WithEntryNameAndEntryInCache_RetrievesFromCache()
         {
             // arrange
-            var entry = new Entry(PostType.BlogPost) { Id = 111, EntryName = "entry-slug", DateSyndicated = DateTime.Now.AddDays(-1)};
+            var entry = new Entry(PostType.BlogPost) { Id = 111, EntryName = "entry-slug", DateSyndicated = DateTime.Now.AddDays(-1) };
             var timeZone = new Mock<ITimeZone>();
             timeZone.Setup(tz => tz.Now).Returns(DateTime.Now);
             var blog = new Mock<Blog>();
@@ -392,7 +392,7 @@ namespace UnitTests.Subtext.Framework.Data
             var tag = new Tag(new KeyValuePair<string, int>("Test", 123));
             var context = new Mock<ISubtextContext>();
             context.Setup(c => c.Blog).Returns(new Blog { Id = 1001 });
-            context.Setup(c => c.Cache["TagsCount10BlogId1001"]).Returns(new[]  { tag });
+            context.Setup(c => c.Cache["TagsCount10BlogId1001"]).Returns(new[] { tag });
             context.Setup(c => c.Repository.GetTopTags(10)).Throws(new Exception("Repository should not have been accessed"));
 
             // act
@@ -409,7 +409,7 @@ namespace UnitTests.Subtext.Framework.Data
             var context = new Mock<ISubtextContext>();
             context.Setup(c => c.Blog).Returns(new Blog { Id = 1001 });
             context.Setup(c => c.Cache["TagsCount10BlogId1001"]).Returns(null);
-            context.Setup(c => c.Repository.GetTopTags(10)).Returns(new Dictionary<string, int>{{"test", 123}});
+            context.Setup(c => c.Repository.GetTopTags(10)).Returns(new Dictionary<string, int> { { "test", 123 } });
 
             // act
             var cachedTags = Cacher.GetTopTags(10, context.Object);
@@ -432,9 +432,8 @@ namespace UnitTests.Subtext.Framework.Data
             var routeData = new RouteData();
             routeData.Values.Add("id", "123");
             var subtextContext = new Mock<ISubtextContext>();
-            subtextContext.SetupRequestContext(httpContext, routeData, new Blog {Id = 123})
-                .Setup(c => c.Repository.GetEntry(123, true, true)).Returns(new Entry(PostType.BlogPost)
-                {Id = 123, Title = "Testing 123"});
+            subtextContext.SetupRequestContext(httpContext, routeData, new Blog { Id = 123 })
+                .Setup(c => c.Repository.GetEntry(123, true, true)).Returns(new Entry(PostType.BlogPost) { Id = 123, Title = "Testing 123" });
 
             //act
             Entry entry = Cacher.GetEntryFromRequest(true, subtextContext.Object);
@@ -454,36 +453,30 @@ namespace UnitTests.Subtext.Framework.Data
             //arrange
             var repository = new Mock<ObjectProvider>();
 
-            var entry = new Entry(PostType.BlogPost) {Id = 123, EntryName = "testing-slug", Title = "Testing 123"};
+            var entry = new Entry(PostType.BlogPost) { Id = 123, EntryName = "testing-slug", Title = "Testing 123" };
             repository.Setup(r => r.GetEntry(123, true, true)).Returns(entry);
             var urlHelper = new Mock<UrlHelper>();
             urlHelper.Setup(u => u.EntryUrl(It.IsAny<Entry>())).Returns("/archive/testing-slug.aspx");
             UnitTestHelper.SetupBlog();
             var response = new Mock<HttpResponseBase>();
-            response.Setup(r => r.End());
-            response.SetupSet(r => r.StatusCode, 301);
-            response.SetupSet(r => r.StatusDescription, "301 Moved Permanently");
-            response.SetupSet(r => r.RedirectLocation, "http://localhost/archive/testing-slug.aspx");
             var httpContext = new Mock<HttpContextBase>();
             httpContext.FakeRequest("~/archive/testing-slug.aspx");
             httpContext.Setup(c => c.Response).Returns(response.Object);
-            
+
             var routeData = new RouteData();
             routeData.Values.Add("id", "123");
             var subtextContext = new Mock<ISubtextContext>();
             subtextContext.SetupRequestContext(httpContext, routeData)
                 .SetupUrlHelper(urlHelper)
                 .SetupRepository(repository.Object)
-                .SetupBlog(new Blog {Host = "localhost"});
+                .SetupBlog(new Blog { Host = "localhost" });
             subtextContext.Setup(c => c.HttpContext).Returns(httpContext.Object);
 
             //act
             Entry cachedEntry = Cacher.GetEntryFromRequest(true /* allowRedirect */, subtextContext.Object);
 
             //assert
-            response.VerifySet(r => r.StatusCode, 301);
-            response.VerifySet(r => r.StatusDescription, "301 Moved Permanently");
-            response.VerifySet(r => r.RedirectLocation, "http://localhost/archive/testing-slug.aspx");
+            response.Verify(r => r.RedirectPermanent("http://localhost/archive/testing-slug.aspx", true));
             Assert.AreEqual(123, cachedEntry.Id);
             Assert.AreEqual("Testing 123", cachedEntry.Title);
         }
@@ -501,9 +494,8 @@ namespace UnitTests.Subtext.Framework.Data
             routeData.Values.Add("slug", "the-slug");
             var subtextContext = new Mock<ISubtextContext>();
             subtextContext.SetupRequestContext(httpContext, routeData)
-                .SetupBlog(new Blog {Id = 1, TimeZoneId = TimeZonesTest.PacificTimeZoneId /* pacific */})
-                .Setup(c => c.Repository.GetEntry("the-slug", true, true)).Returns(new Entry(PostType.BlogPost)
-                {Id = 123, EntryName = "the-slug", Title = "Testing 123"});
+                .SetupBlog(new Blog { Id = 1, TimeZoneId = TimeZonesTest.PacificTimeZoneId /* pacific */})
+                .Setup(c => c.Repository.GetEntry("the-slug", true, true)).Returns(new Entry(PostType.BlogPost) { Id = 123, EntryName = "the-slug", Title = "Testing 123" });
 
             //act
             Entry entry = Cacher.GetEntryFromRequest(true, subtextContext.Object);
@@ -553,7 +545,7 @@ namespace UnitTests.Subtext.Framework.Data
             var requestContext = new RequestContext(new Mock<HttpContextBase>().Object, routeData);
             var subtextContext = new Mock<ISubtextContext>();
             subtextContext.Setup(c => c.RequestContext).Returns(requestContext);
-            subtextContext.Setup(c => c.Blog).Returns(new Blog {Id = 123});
+            subtextContext.Setup(c => c.Blog).Returns(new Blog { Id = 123 });
             subtextContext.Setup(c => c.Repository.GetLinkCategory(99, true)).Returns((LinkCategory)null);
             subtextContext.Setup(c => c.Cache[It.IsAny<string>()]).Returns(null);
 
@@ -573,9 +565,9 @@ namespace UnitTests.Subtext.Framework.Data
             var requestContext = new RequestContext(new Mock<HttpContextBase>().Object, routeData);
             var subtextContext = new Mock<ISubtextContext>();
             subtextContext.Setup(c => c.RequestContext).Returns(requestContext);
-            subtextContext.Setup(c => c.Blog).Returns(new Blog {Id = 123});
+            subtextContext.Setup(c => c.Blog).Returns(new Blog { Id = 123 });
             subtextContext.Setup(c => c.Repository.GetLinkCategory(99, true))
-                .Returns(new LinkCategory {Id = 99, Title = "this is a test"});
+                .Returns(new LinkCategory { Id = 99, Title = "this is a test" });
             subtextContext.Setup(c => c.Cache[It.IsAny<string>()]).Returns(null);
 
             //act
@@ -594,9 +586,9 @@ namespace UnitTests.Subtext.Framework.Data
             var requestContext = new RequestContext(new Mock<HttpContextBase>().Object, routeData);
             var subtextContext = new Mock<ISubtextContext>();
             subtextContext.Setup(c => c.RequestContext).Returns(requestContext);
-            subtextContext.Setup(c => c.Blog).Returns(new Blog {Id = 123});
+            subtextContext.Setup(c => c.Blog).Returns(new Blog { Id = 123 });
             subtextContext.Setup(c => c.Repository.GetLinkCategory("this-is-a-test", true))
-                .Returns(new LinkCategory {Id = 99, Title = "this is a test"});
+                .Returns(new LinkCategory { Id = 99, Title = "this is a test" });
             subtextContext.Setup(c => c.Cache[It.IsAny<string>()]).Returns(null);
 
             //act
