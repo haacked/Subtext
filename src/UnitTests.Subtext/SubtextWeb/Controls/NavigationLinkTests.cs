c1a3aa70236300b98c8fc29e922c55d41067be02
haacked <haacked@483c6c4e-18e1-11de-b208-e5a9466b8568>
2010-12-31 18:42:53 +0000

    Adding some images missing from a patch. Also forgot to actually add the unit test file. Adding that now.

---
+++ src/UnitTests.Subtext/SubtextWeb/Controls/NavigationLinkTests.cs
@@ -0,0 +1,87 @@
+﻿using MbUnit.Framework;
+using Subtext.Web.Controls;
+
+namespace UnitTests.Subtext.SubtextWeb.Controls
+{
+    [TestFixture]
+    public class NavigationLinkTests
+    {
+        [Test]
+        public void IsRequestForSamePage_WithRequestForRootAndNavigateUrlAsSlash_ReturnsTrue()
+        {
+            // arrange
+            var navigationLink = new NavigationLink();
+
+            // act
+            bool isSamePage = navigationLink.IsRequestForSamePage("/", "/");
+
+            // assert
+            Assert.IsTrue(isSamePage);
+        }
+
+        [Test]
+        public void IsRequestForSamePage_WithRequestForRootAndNavigateUrlAsDefaultAspx_ReturnsTrue()
+        {
+            // arrange
+            var navigationLink = new NavigationLink();
+
+            // act
+            bool isSamePage = navigationLink.IsRequestForSamePage("/Default.aspx", "/");
+
+            // assert
+            Assert.IsTrue(isSamePage);
+        }
+
+        [Test]
+        public void IsRequestForSamePage_WithRequestForDefaultAspxRootAndNavigateUrlAsSlash_ReturnsTrue()
+        {
+            // arrange
+            var navigationLink = new NavigationLink { NavigateUrl = "~/" };
+
+            // act
+            bool isSamePage = navigationLink.IsRequestForSamePage("/", "/Default.aspx");
+
+            // assert
+            Assert.IsTrue(isSamePage);
+        }
+
+        [Test]
+        public void IsRequestForSamePage_WithRequestForDirectoryWithMatchingNavigateUrl_ReturnsTrue()
+        {
+            // arrange
+            var navigationLink = new NavigationLink();
+
+            // act
+            bool isSamePage = navigationLink.IsRequestForSamePage("/Foo/Bar/", "/Foo/Bar/");
+
+            // assert
+            Assert.IsTrue(isSamePage);
+        }
+
+        [Test]
+        public void IsRequestForSamePage_WithDefaultAspxRequestForDirectoryWithMatchingNavigateUrl_ReturnsTrue()
+        {
+            // arrange
+            var navigationLink = new NavigationLink();
+
+            // act
+            bool isSamePage = navigationLink.IsRequestForSamePage("/Foo/Bar/", "/Foo/Bar/Default.aspx");
+
+            // assert
+            Assert.IsTrue(isSamePage);
+        }
+
+        [Test]
+        public void IsRequestForSamePage_WithRequestForDirectoryWithMatchingDefaultAspxNavigateUrl_ReturnsTrue()
+        {
+            // arrange
+            var navigationLink = new NavigationLink();
+
+            // act
+            bool isSamePage = navigationLink.IsRequestForSamePage("/Foo/Bar/Default.aspx", "/Foo/Bar/");
+
+            // assert
+            Assert.IsTrue(isSamePage);
+        }
+    }
+}
